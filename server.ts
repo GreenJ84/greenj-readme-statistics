e9509eda01af8e0283423ecc0d4671008fc5be33
Jesse Greenough <jesse.greenough84@gmail.com>
2023-03-10 03:41:59 -0800

    Update server to function API

--- server.ts
+++ server.ts
@@ -1,9 +1,10 @@
 import express from 'express';
 import cors from 'cors';
+import { LeetCodeRoutes } from './src/routes/leetcode.routes';
 
 const PORT = 8000
 
-const app = express()
+const app = express();
 app.use(
     express.json(),
     express.urlencoded({ extended: true })
@@ -12,11 +13,14 @@ app.use(
 app.use(
     cors({
         origin: '*',
-        preflightContinue: true,
         methods: 'GET'
     }),
-)
+);
 
+LeetCodeRoutes(app)
 
 app.listen(PORT,
-    () => console.log(`Server is running on port ${PORT}`))
+    () => console.log(`Server is running on port ${PORT}`)
+)
+
+export default {}
