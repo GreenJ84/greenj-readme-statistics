962c86b1c6039173cc5f694459efe0bf5af77a3e
Jesse Greenough <jesse.greenough84@gmail.com>
2023-03-13 19:46:13 -0700

    Update controller seperating concerns

--- src/controllers/leetcode.controller.ts
+++ src/controllers/leetcode.controller.ts
@@ -1,45 +1,22 @@
 /** @format */
 
 export { };
-import fs from 'fs';
 import { Request, Response } from "express";
-import { gql } from "graphql-tag";
-import { ApolloClient, InMemoryCache } from "@apollo/client";
-import { BASE_URL } from "leetcode-query";
+import { profile } from '../leetcode/profile';
 
-export const getLeetcodeProfileStats = async (req: Request, res: Response) => {
+export const getProfileStats = async (req: Request, res: Response) => {
     const { username } = req.params;
     const { } = req.query;
-
     if (!username) {
-        res.send({ message: 'No username found on API Call', error: "Missing username parameter.", error_code: 400})
+        res.status(400).send({ message: 'No username found on API Call', error: "Missing username parameter.", error_code: 400})
     }
 
-    const endpoint = BASE_URL;
-    const query = gql(
-        fs.readFileSync('../leetcode/graphql/leetcode-profile.graphql', 'utf8')
-    );
-    const client = new ApolloClient({
-        uri: endpoint,
-        cache: new InMemoryCache(),
-    });
-
-    try {
-        client.query({ query })
-            .then(result => {
-                res.status(300).send(result)
-            })
-            .catch(err => {
-                console.error(err)
-                res.send({
-                    message: "Something has gone wrong with the API",
-                    error_code: 400,
-                    error: err
-                })
-            });
-    }
-    catch (err) {
-        console.log("an error has occured", err);
-        res.status(500).send(`An error occurred while retrieving data from the external API: ${err}`);
-    }
+    profile(username!).then((data) => {
+        if ("error" in data && "error_code" in data) {
+            res.status(400).send(data);
+        } else {
+            res.status(200).send({ data: data });
+        }
+    })
 }
+
