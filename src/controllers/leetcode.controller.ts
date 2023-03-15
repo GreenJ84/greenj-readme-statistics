12eb49a1beda13b8a81ee2f54660a869d4c7cac2
Jesse Greenough <jesse.greenough84@gmail.com>
2023-03-15 01:08:01 -0700

    Update controller route with types
    Sort by profile data and non-profile data
    
    Implement path retrieval for request type

--- src/controllers/leetcode.controller.ts
+++ src/controllers/leetcode.controller.ts
@@ -2,21 +2,89 @@
 
 export { };
 import { Request, Response } from "express";
-import { profile } from '../leetcode/profile';
+import { getDaily } from "../leetcode/daily";
+import { setQuery } from '../leetcode/profile';
 
 export const getProfileStats = async (req: Request, res: Response) => {
     const { username } = req.params;
     const { } = req.query;
+    const type = req.path.split("/")[2]!;
+    console.log(type);
     if (!username) {
-        res.status(400).send({ message: 'No username found on API Call', error: "Missing username parameter.", error_code: 400})
+        res.status(400).send({ message: 'No username found on API Call', error: "Missing username parameter.", error_code: 400 })
     }
 
-    profile(username!).then((data) => {
+    setQuery(username!, type).then((data) => {
         if ("error" in data && "error_code" in data) {
             res.status(400).send(data);
         } else {
-            res.status(200).send({ data: data });
+            res.status(200).send(data);
         }
     })
-}
+};
+
+export const getQuestionsAnswered = async (req: Request, res: Response) => {
+    const { username } = req.params;
+    const { } = req.query;
+    const type = req.path.split("/")[2]!;
+    console.log(type);
+    if (!username) {
+        res.status(400).send({ message: 'No username found on API Call', error: "Missing username parameter.", error_code: 400 })
+    }
+
+    setQuery(username!, type).then((data) => {
+        if ("error" in data && "error_code" in data) {
+            res.status(400).send(data);
+        } else {
+            res.status(200).send(data);
+        }
+    })
+};
+
+export const getBadges = async (req: Request, res: Response) => {
+    const { username } = req.params;
+    const { } = req.query;
+    const type = req.path.split("/")[2]!;
+    console.log(type);
+    if (!username) {
+        res.status(400).send({ message: 'No username found on API Call', error: "Missing username parameter.", error_code: 400 })
+    }
+
+    setQuery(username!, type).then((data) => {
+        if ("error" in data && "error_code" in data) {
+            res.status(400).send(data);
+        } else {
+            res.status(200).send(data);
+        }
+    })
+};
+
+export const getRecentSubmitions = async (req: Request, res: Response) => {
+    const { username } = req.params;
+    const { } = req.query;
+    const type = req.path.split("/")[2]!;
+    console.log(type);
+    if (!username) {
+        res.status(400).send({ message: 'No username found on API Call', error: "Missing username parameter.", error_code: 400 })
+    }
+
+    setQuery(username!, type).then((data) => {
+        if ("error" in data && "error_code" in data) {
+            res.status(400).send(data);
+        } else {
+            res.status(200).send(data);
+        }
+    })
+};
+
+export const getDailyQuestion = async (req: Request, res: Response) => {
+    const { } = req.query;
+    getDaily().then((data) => {
+        if ("error" in data && "error_code" in data) {
+            res.status(400).send(data);
+        } else {
+            res.status(200).send(data);
+        }
+    })
+};
 
