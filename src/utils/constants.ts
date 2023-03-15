a41361d18a0ec048991adb012dba9a7230cb16af
Jesse Greenough <jesse.greenough84@gmail.com>
2023-03-15 01:34:56 -0700

    Update constant with generics and github api

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,4 +1,24 @@
+import { DocumentNode } from "graphql";
+
+// LeetCode
 export const CRED_URL = "https://leetcode.com";
 export const GRAPHQL_URL = "https://leetcode.com/graphql/";
 export const GRAPHQL_URL_CN = "https://leetcode.cn/graphql/";
 export const USER_AGENT = "Mozilla/5.0 LeetCode API";
+
+//GitHub
+export const GIT_URL = "https://api.github.com/graphql/";
+
+
+
+// Extras
+export interface GraphQLError {
+    message: string,
+    error: any,
+    error_code: number
+}
+
+export interface GraphQLQuery {
+    variables?: { [key: string]: unknown };
+    query: DocumentNode;
+}
