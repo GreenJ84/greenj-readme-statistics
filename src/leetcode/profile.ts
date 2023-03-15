fd615ca5b91f8ad88ca168e49e499c0e8d446242
Jesse Greenough <jesse.greenough84@gmail.com>
2023-03-15 01:07:35 -0700

    Create match function to grab filepath based on type

--- src/leetcode/profile.ts
+++ src/leetcode/profile.ts
@@ -1,15 +1,25 @@
 import fs from 'fs';
 import { gql } from "graphql-tag";
+import { match } from 'ts-pattern';
 import * as leetcode from '../leetcode/query';
 import { GRAPHQL_URL } from '../utils/constants';
 import { get_csrf } from '../utils/credentials';
 import { GraphQLError, LeetCodeGraphQLResponse } from './leetcodeTypes';
 
-export const profile = async (username: string) => {
+const getGraph = (type: string): string => {
+    const graph = match(type)
+        .with("stats", () => {return "src/leetcode/graphql/leetcode-profile.graphql"})
+        .with("badges", () => {return "src/leetcode/graphql/leetcode-badges.graphql"})
+        .with("questions_solved", () => {return "src/leetcode/graphql/leetcode-questions-answered.graphql"})
+        .with("recent-questions", () => { return "src/leetcode/graphql/leetcode-recent-submissions.graphql" })
+        .run()
+    return graph
+}
+export const setQuery = async (username: string, type: string) => {
+    const path = getGraph(type);
     const graphql = gql(
-        fs.readFileSync('src/leetcode/graphql/leetcode-profile.graphql', 'utf8')
+        fs.readFileSync(path, 'utf8')
     );
-
     const csrf_credential: string = await get_csrf()
         .then((result) => result.toString());
 
