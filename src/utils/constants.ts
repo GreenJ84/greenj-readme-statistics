dbf318f17c69041b47ec887b9e00d3af4504dedc
Jesse Greenough <jesse.greenough84@gmail.com>
2023-03-14 23:52:19 -0700

    Add user agent to constants

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,3 +1,4 @@
-export const CRED_URL = "https://leetcode.com"
-export const GRAPHQL_URL = "https://leetcode.com/graphql/"
-export const GRAPHQL_URL_CN = "https://leetcode.cn/graphql/"
+export const CRED_URL = "https://leetcode.com";
+export const GRAPHQL_URL = "https://leetcode.com/graphql/";
+export const GRAPHQL_URL_CN = "https://leetcode.cn/graphql/";
+export const USER_AGENT = "Mozilla/5.0 LeetCode API";
