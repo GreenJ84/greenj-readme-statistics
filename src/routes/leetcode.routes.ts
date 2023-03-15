e533200aa560e06e82f29aa05c273095d4d3f839
Jesse Greenough <jesse.greenough84@gmail.com>
2023-03-15 01:09:20 -0700

    Implement all routes for Leetcode data retrieval

--- src/routes/leetcode.routes.ts
+++ src/routes/leetcode.routes.ts
@@ -7,13 +7,17 @@ export const LeetCodeRoutes = (app: express.Application) => {
         LeetcodeController.getProfileStats
     )
     // Display earned leetcode badges
-    app.get('/leetcode/badges/:user', (req, res) => {
-        res.json(`Hello ${req.params.user!}`)
-    })
+    app.get('/leetcode/badges/:username',
+    LeetcodeController.getBadges
+)
     // Get number of questions solved in total and by level
-    app.get('/leetcode/questions_solved/:user')
+    app.get('/leetcode/questions_solved/:username',
+    LeetcodeController.getQuestionsAnswered
+)
     // Display top 5 most recent questions solved and their languages
-    app.get('/leetcode/recent-questions/:user')
+    app.get('/leetcode/recent-questions/:username',
+    LeetcodeController.getRecentSubmitions
+)
     // Get a random leetcode problem title
-    app.get('/leetcode/random-question')
+    app.get('/leetcode/daily-question', LeetcodeController.getDailyQuestion)
 }
