1ebc1b31bbb52219b4e84f6d1dcb596e51939ac2
Jesse Greenough <jesse.greenough84@gmail.com>
2023-03-15 01:06:52 -0700

    Create daily question query set-up

---
+++ src/leetcode/daily.ts
@@ -0,0 +1,32 @@
+import fs from 'fs';
+import gql from "graphql-tag";
+import { GRAPHQL_URL } from '../utils/constants';
+import { get_csrf } from '../utils/credentials';
+import { GraphQLError, LeetCodeGraphQLResponse } from './leetcodeTypes';
+import * as leetcode from './query';
+
+export const getDaily = async () => {
+    const graphql = gql(
+        fs.readFileSync("src/leetcode/graphql/leetcode-daily-question.graphql", 'utf8')
+    );
+    const csrf_credential: string = await get_csrf()
+        .then((result) => result.toString());
+
+    const data = await leetcode.leetcodeGraphQL(
+        {
+            query: graphql,
+            variables: {}
+        },
+        GRAPHQL_URL,
+        csrf_credential
+    )
+        .then((res) => res as LeetCodeGraphQLResponse)
+        .catch((err) => {
+            return {
+                message: "",
+                error: err,
+                error_code: 400, 
+            } as GraphQLError
+        })
+    return data
+}
