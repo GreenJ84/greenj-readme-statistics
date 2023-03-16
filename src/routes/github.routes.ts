505be372118fa80067165d49ee81a69e337afbc0
Jesse Greenough <jesse.greenough84@gmail.com>
2023-03-16 15:19:25 -0700

    Implement controller fuctions on api routes

--- src/routes/github.routes.ts
+++ src/routes/github.routes.ts
@@ -1,10 +1,21 @@
 import express from "express";
+import * as Github from '../controllers/github.controller';
 
 export const GithubRoutes = (app: express.Application) => {
     // Display github stats in trophies
-    app.get('/github/trophies')
+    app.get('/github/trophies',
+        Github.getProfileTrophies
+    )
     // Display github stats in Modal
-    app.get('/github/stats')
+    app.get('/github/stats',
+        Github.getProfileStats
+    )
     // Display most used github languages
-    app.get('/github/languages')
+    app.get('/github/languages',
+        Github.getTopLangs
+    )
+    // Display github contributions streak
+    app.get('/github/streak',
+        Github.getCommitStreak
+    )
 }
