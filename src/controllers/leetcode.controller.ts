3e208ac33932c5fff0e70ff09b0efd22aa5ea9ac
Jesse Greenough <jesse.greenough84@gmail.com>
2023-03-16 15:13:31 -0700

    Implement preQuery functionality for routes

--- src/controllers/leetcode.controller.ts
+++ src/controllers/leetcode.controller.ts
@@ -1,80 +1,24 @@
 /** @format */
 
-export { };
 import { Request, Response } from "express";
 import { getDaily } from "../leetcode/daily";
-import { setQuery } from '../leetcode/profile';
+import { preQuery } from '../leetcode/profile';
 
 export const getProfileStats = async (req: Request, res: Response) => {
-    const { username } = req.params;
-    const { } = req.query;
-    const type = req.path.split("/")[2]!;
-    console.log(type);
-    if (!username) {
-        res.status(400).send({ message: 'No username found on API Call', error: "Missing username parameter.", error_code: 400 })
-    }
+    preQuery(req, res);
 
-    setQuery(username!, type).then((data) => {
-        if ("error" in data && "error_code" in data) {
-            res.status(400).send(data);
-        } else {
-            res.status(200).send(data);
-        }
-    })
 };
 
 export const getQuestionsAnswered = async (req: Request, res: Response) => {
-    const { username } = req.params;
-    const { } = req.query;
-    const type = req.path.split("/")[2]!;
-    console.log(type);
-    if (!username) {
-        res.status(400).send({ message: 'No username found on API Call', error: "Missing username parameter.", error_code: 400 })
-    }
-
-    setQuery(username!, type).then((data) => {
-        if ("error" in data && "error_code" in data) {
-            res.status(400).send(data);
-        } else {
-            res.status(200).send(data);
-        }
-    })
+    preQuery(req, res);
 };
 
 export const getBadges = async (req: Request, res: Response) => {
-    const { username } = req.params;
-    const { } = req.query;
-    const type = req.path.split("/")[2]!;
-    console.log(type);
-    if (!username) {
-        res.status(400).send({ message: 'No username found on API Call', error: "Missing username parameter.", error_code: 400 })
-    }
-
-    setQuery(username!, type).then((data) => {
-        if ("error" in data && "error_code" in data) {
-            res.status(400).send(data);
-        } else {
-            res.status(200).send(data);
-        }
-    })
+    preQuery(req, res);
 };
 
 export const getRecentSubmitions = async (req: Request, res: Response) => {
-    const { username } = req.params;
-    const { } = req.query;
-    const type = req.path.split("/")[2]!;
-    console.log(type);
-    if (!username) {
-        res.status(400).send({ message: 'No username found on API Call', error: "Missing username parameter.", error_code: 400 })
-    }
-
-    setQuery(username!, type).then((data) => {
-        if ("error" in data && "error_code" in data) {
-            res.status(400).send(data);
-        } else {
-            res.status(200).send(data);
-        }
-    })
+    preQuery(req, res);
 };
 
 export const getDailyQuestion = async (req: Request, res: Response) => {
