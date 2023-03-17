15d398eaadd280a7a001043c4c52a2a9ad6d84ca
Jesse Greenough <jesse.greenough84@gmail.com>
2023-03-17 04:50:33 -0700

    Update routes to take username paramter

--- src/routes/github.routes.ts
+++ src/routes/github.routes.ts
@@ -3,19 +3,19 @@ import * as Github from '../controllers/github.controller';
 
 export const GithubRoutes = (app: express.Application) => {
     // Display github stats in trophies
-    app.get('/github/trophies',
+    app.get('/github/trophies/:username',
         Github.getProfileTrophies
     )
     // Display github stats in Modal
-    app.get('/github/stats',
+    app.get('/github/stats/:username',
         Github.getProfileStats
     )
     // Display most used github languages
-    app.get('/github/languages',
+    app.get('/github/languages/:username',
         Github.getTopLangs
     )
     // Display github contributions streak
-    app.get('/github/streak',
+    app.get('/github/streak/:username',
         Github.getCommitStreak
     )
 }
