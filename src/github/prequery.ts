9813cefa3e7c53ff614d8552a2ca8135ad6327d1
Jesse Greenough <jesse.greenough84@gmail.com>
2023-03-18 16:48:27 -0700

    Start setting up graph response parsing

--- src/github/prequery.ts
+++ src/github/prequery.ts
@@ -4,6 +4,8 @@ import { gql } from "graphql-tag";
 import { match } from 'ts-pattern';
 import * as github from './query';
 import { GraphQLError } from '../utils/constants';
+import { GraphQLResponse } from './githubTypes';
+import { streakParse } from './apiParser';
 
 const getGraph = (type: string): string => {
     const graph = match(type)
@@ -15,7 +17,41 @@ const getGraph = (type: string): string => {
     return graph
 }
 
-export const preQery = async (req: Request, res: Response) => {
+const getResponseParse = (type: string): Function => {
+    const parseFunc = match(type)
+        // .with("stats", () => {return statsParse})
+        // .with("trophies", () => {return statsParse})
+        // .with("languages", () => {return langsParse})
+        .with("streak", () => {return streakParse})
+        .run()
+    return parseFunc
+}
+
+
+export const setQuery = async (username: string, type: string): Promise<GraphQLError| GraphQLResponse> => {
+    const path = getGraph(type);
+    const graphql = gql(
+        fs.readFileSync(path, 'utf8')
+    );
+    const data = await github.githubGraphQL(
+        {
+            query: graphql,
+            variables: { login: username }
+        })
+        .then((res) => res as GraphQLResponse)
+        .catch((err) => {
+            return {
+                message: "Internal server error",
+                error: err,
+                error_code: 500,
+            } as GraphQLError;
+        })
+    const parse = getResponseParse(type);
+    parse(data);
+    return data;
+}
+
+export const preQery = async (req: Request, res: Response): Promise<GraphQLError | GraphQLResponse> => {
     const { username } = req.params;
     const type = req.path.split("/")[2]!;
 
@@ -27,34 +63,16 @@ export const preQery = async (req: Request, res: Response) => {
                 error_code: 400
             })
     }
-    setQuery(username!, type).then((data) => {
-        if ("error" in data && "error_code" in data) {
-            res.status(400).send(data);
-        } else {
-            res.status(200).send(data);
-        }
-    })
-};
-
-export const setQuery = async (username: string, type: string) => {
-    const path = getGraph(type);
-    const graphql = gql(
-        fs.readFileSync(path, 'utf8')
-    );
-
-    const data = await github.githubGraphQL(
-        {
-            query: graphql,
-            variables: { login: username }
-        },
-    )
-        .then((res) => res as any)
+    const data = setQuery(username!, type)
+        .then((data) => {
+            return data as GraphQLResponse;
+        })
         .catch((err) => {
             return {
                 message: "",
                 error: err,
-                error_code: 400, 
-            } as GraphQLError
+                error_code: 400,
+            } as GraphQLError;
         })
-    return data
+    return data;
 }
