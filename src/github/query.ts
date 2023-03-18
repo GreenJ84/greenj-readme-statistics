720d7b475b5c2c4eb824ed946e5dc1beb1d8d390
Jesse Greenough <jesse.greenough84@gmail.com>
2023-03-18 16:48:05 -0700

    Update github query handling and return types

--- src/github/query.ts
+++ src/github/query.ts
@@ -1,8 +1,9 @@
 import { ApolloClient, InMemoryCache } from "@apollo/client";
 import { GraphQLQuery, GraphQLError, GIT_URL } from "../utils/constants";
+import { GraphQLResponse } from "./githubTypes";
 const token = "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";
 
-export async function githubGraphQL(query: GraphQLQuery): Promise<any> {
+export async function githubGraphQL(query: GraphQLQuery): Promise<GraphQLError | GraphQLResponse> {
     const client = new ApolloClient({
         uri: GIT_URL,
         cache: new InMemoryCache(),
@@ -12,38 +13,24 @@ export async function githubGraphQL(query: GraphQLQuery): Promise<any> {
         "Content-Type": "application/json",
     };
     
-    try {
-        const result = await client.query(
-            {
-                ...query,
-                context: {
-                    headers,
-                    method: 'GET',
-                }
-            }
-        )
-            .then(result => {
-                return result.data as any
-            })
-            .catch(err => {
-                return {
-                    data: {
-                        message: "An error occurred while retrieving data from the external API",
-                        error_code: 500,
-                        error: err
-                    } as GraphQLError
-                }
-            });
-        return result;
-    }
-    catch (err) {
-        console.log("an error has occured", err);
-        return {
-            data: {
-                message: "An error occurred while connecting to the external API",
-                error_code: 500,
-                error: err
+    const result = await client.query(
+        {
+            ...query,
+            context: {
+                headers,
+                method: 'GET',
             }
         }
-    }
+    )
+        .then(result => {
+            return result.data as GraphQLResponse
+        })
+        .catch(err => {
+            return {
+                    message: "An error occurred while retrieving data from the external API",
+                    error_code: 500,
+                    error: err
+                } as GraphQLError
+        });
+    return result;
 };
