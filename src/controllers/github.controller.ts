49462f49320b2f1d50f52b40c5d991bc831f0541
Jesse Greenough <jesse.greenough84@gmail.com>
2023-03-19 00:22:19 -0700

    Update data handling for controller

--- src/controllers/github.controller.ts
+++ src/controllers/github.controller.ts
@@ -1,5 +1,8 @@
 import { Request, Response } from "express";
-import { preQery } from "../github/prequery";
+// import { streakCardSetup } from "../github/cards/streak-card";
+import { GraphQLError } from "../utils/constants";
+import { preQery } from "../github/query";
+import {  ReadMeData } from "../github/githubTypes";
 
 export const getProfileStats = async (req: Request, res: Response) => {
     preQery(req, res);
@@ -10,7 +13,16 @@ export const getProfileTrophies = async (req: Request, res: Response) => {
 };
 
 export const getCommitStreak = async (req: Request, res: Response) => {
-    preQery(req, res);
+    const data: ReadMeData | GraphQLError = await preQery(req, res)
+        .then((data: ReadMeData | GraphQLError) => {
+            return data;
+        })
+    if ((data as GraphQLError).error !== undefined) {
+        res.status(400).send(data);
+    }
+    res.status(200).send(data);
+
+    // const card = streakCardSetup(req);
 };
 
 export const getTopLangs = async (req: Request, res: Response) => {
