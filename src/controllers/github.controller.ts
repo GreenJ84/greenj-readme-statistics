b6fc0c995423b896edc0ee7ce87da24dafc6b456
Jesse Greenough <jesse.greenough84@gmail.com>
2023-03-19 01:27:09 -0700

    Implement card building functionality

--- src/controllers/github.controller.ts
+++ src/controllers/github.controller.ts
@@ -1,8 +1,8 @@
 import { Request, Response } from "express";
-// import { streakCardSetup } from "../github/cards/streak-card";
+import { streakCardSetup } from "../github/cards/streak-card";
 import { GraphQLError } from "../utils/constants";
 import { preQery } from "../github/query";
-import {  ReadMeData } from "../github/githubTypes";
+import {  ReadMeData, STREAKTYPE } from "../github/githubTypes";
 
 export const getProfileStats = async (req: Request, res: Response) => {
     preQery(req, res);
@@ -13,16 +13,17 @@ export const getProfileTrophies = async (req: Request, res: Response) => {
 };
 
 export const getCommitStreak = async (req: Request, res: Response) => {
-    const data: ReadMeData | GraphQLError = await preQery(req, res)
+    const data: STREAKTYPE = await preQery(req, res)
         .then((data: ReadMeData | GraphQLError) => {
-            return data;
+            if ((data as GraphQLError).error !== undefined) {
+                res.status(400).send(data);
+            }
+            return data as unknown as STREAKTYPE;
         })
-    if ((data as GraphQLError).error !== undefined) {
-        res.status(400).send(data);
-    }
-    res.status(200).send(data);
-
-    // const card = streakCardSetup(req);
+        
+    const card: string = streakCardSetup(req, data);
+    console.log(card)
+    res.status(200).send(card);
 };
 
 export const getTopLangs = async (req: Request, res: Response) => {
