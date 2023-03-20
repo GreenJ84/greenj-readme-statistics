78d9a9102515aec220b2fe67ba50ae8acb2c86a4
Jesse Greenough <jesse.greenough84@gmail.com>
2023-03-19 17:00:12 -0700

    Update dates used to start probe
    Update the return of data

--- src/github/streakHandle.ts
+++ src/github/streakHandle.ts
@@ -2,11 +2,11 @@ import fs from 'fs';
 import { Request, Response } from "express";
 import gql from "graphql-tag";
 
-import { GraphQLResponse, StreakProbe, StreakResponse } from "./githubTypes";
+import { GraphQLResponse, StreakProbe } from "./githubTypes";
 import { githubGraphQL } from "./query";
 import { GraphQLError } from '../utils/constants';
 
-export const handleProbe = (req: Request, res: Response): [string, number[]] => {
+export const handleProbe = async (req: Request, res: Response): Promise<[string, number[]] | boolean> => {
     const now = new Date().toISOString()
     const today = now.slice(0, 19);
     const year = now.slice(0,4)
@@ -15,8 +15,8 @@ export const handleProbe = (req: Request, res: Response): [string, number[]] => 
     );
     const variables = {
         login: req.params.username!,
-        start: today,
-        end: `${year}-01-01T00:00:00Z`
+        start: `${year}-01-01T00:00:00Z`,
+        end: today
     }
     const data = await githubGraphQL(
         {
@@ -31,17 +31,14 @@ export const handleProbe = (req: Request, res: Response): [string, number[]] => 
                 error_code: 500,
             } as GraphQLError;
         })
-    // Return API errors if they have occured
+    // Return API errors if they have occured and flag call termination
     if ((data as GraphQLError).error !== undefined) {
         res.status(400).send(data);
+        return false;
+    } else {
+        return [
+            (data as unknown as StreakProbe).user.createdAt,
+            [...(data as unknown as StreakProbe).user.contributionsCollection.contributionYears].sort()];
     }
-
-    return [
-        (data as unknown as StreakProbe).user.createdAt,
-        (data as unknown as StreakProbe).user.contributionsCollection.contributionYears.sort()];
     
-}
-
-export const handleStreakMulti = (data: StreakResponse): => {
-
 }
