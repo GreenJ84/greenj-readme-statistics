07f1e05d1a78ff53eaeb0aac49d229868e06be23
Jesse Greenough <jesse.greenough84@gmail.com>
2023-03-19 19:16:52 -0700

    Interface updates for new fields and new response types

--- src/github/githubTypes.ts
+++ src/github/githubTypes.ts
@@ -3,10 +3,13 @@ import { THEMETYPE } from "../utils/themes"
 export enum GraphQLResponse {
     StreakResponse,
     StreakProbe,
+    StatsResponse
 }
 
 export enum ReadMeData {
     STREAKTYPE,
+    STATTYPE,
+    LANGTYPE
 }
 
 export interface StreakProbe{
@@ -17,13 +20,20 @@ export interface StreakProbe{
         }
     }
 };
+
+
 export interface StreakResponse {
     user: {
         createdAt: string
         contributionsCollection: {
             contributionYears: number[]
             contributionCalendar: {
-                weeks: contrWeek[]
+                weeks: {
+                    contributionDays: {
+                        contributionCount: number
+                        date: string
+                    }[]
+                }[]
             }
         }
     }
@@ -51,15 +61,50 @@ export interface STREAKTYPE {
     theme: THEMETYPE
 }
 
+export interface StatsResponse{
+    user: {
+        name: string
+        contributionsCollection: {
+            totalCommitContributions: number
+            restrictedContributionsCount: number
+        }
+        repositoriesContributedTo: {
+            totalCount: number
+        }
+        pullRequests: {
+            totalCount: number
+        }
+        issues: {
+            totalCount: number
+        }
+        followers: {
+            totalCount: number
+        }
+        repositories: {
+            totalCount: number
+            nodes: {
+                name: string
+                stargazers: {
+                    totalCount: number
+                }
+            }[]
+        }
+    }
+}
+
+
 export interface STATTYPE{
-    grade: string
-    totalStars: number
-    totalCommits: number
-    totalPR: number
-    totalIssues: number
+    grade?: string
+    stars: number
+    followers: number
+    commits: number
+    PR: number
+    issues: number
     contributedTo: number,
+    repos: number
     theme: THEMETYPE
 }
+
 
 export interface LANGTYPE {
     name: string
