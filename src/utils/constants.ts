f7ffa010134de7a7da093b992f3d5317e8eb67d2
Jesse Greenough <jesse.greenough84@gmail.com>
2023-03-21 13:06:39 -0700

    Update leetcode constants

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -2,8 +2,8 @@ import { DocumentNode } from "graphql";
 
 // LeetCode
 export const CRED_URL = "https://leetcode.com";
-export const GRAPHQL_URL = "https://leetcode.com/graphql/";
-export const GRAPHQL_URL_CN = "https://leetcode.cn/graphql/";
+export const GRAPHQL_URL = "https://leetcode.com/graphql";
+export const GRAPHQL_URL_CN = "https://leetcode.cn/graphql";
 export const USER_AGENT = "Mozilla/5.0 LeetCode API";
 
 //GitHub
