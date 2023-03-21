c3c9b73e193afc22763a91748f3c5f1e553e2b7e
Jesse Greenough <jesse.greenough84@gmail.com>
2023-03-21 04:13:04 -0700

    Update leetcode repsonse and data types

--- src/leetcode/leetcodeTypes.ts
+++ src/leetcode/leetcodeTypes.ts
@@ -1,153 +1,130 @@
-import { GraphQLError } from '../utils/constants';
+import { THEMETYPE } from "../utils/themes";
 
-
-export interface UserProfile {
-    allQuestionsCount: AllQuestionsCount[];
-    matchedUser: MatchedUser | null;
-    recentSubmissionList: RecentSubmissionList | null;
+export interface ProbeResponse{
+    matchedUser: {
+        userCalendar: {
+            activeYears: number[]
+        }
+    }
 }
 
-    export interface AllQuestionsCount {
+export interface StreakResponse {
+    allQuestionsCount: AllQuestionsCount[];
+    matchedUser: {
+        userCalendar: {
+            streak: number
+            totalActiveDays: number
+        }
+        submitStats: {
+            acSubmissionNum: {
+                difficulty: string
+                count: number
+                submissions: number
+            }
+        }
+    }
+}
+export interface STREAKDATA{
+    streak: [number, number]
+    totalActive: number
+    completion: number
+}
+
+
+
+
+export interface ProfileResponse {
+    allQuestionsCount: AllQuestionsCount[];
+    matchedUser: {
+        contributions: {
+            points: number
+        }
+        profile: {
+            starRating: number
+            reputation: number
+        }
+        submitStats: {
+            acSubmissionNum: {
+                difficulty: string
+                count: number
+                submissions: number
+            }
+        }
+        badges: {
+            id: string
+        }[];
+    }
+}
+    interface AllQuestionsCount {
         difficulty: string;
         count: number;
     }
+export interface PROFILEDATA {
+    title: string
+    completion: number
+    reputation: number
+    stars: number
+    badges: number
+    contributions: number
+    grade: string
+    theme: THEMETYPE
+}
 
-    export interface MatchedUser {
-        username: string;
-        socialAccounts: unknown;
-        githubUrl: null;
-        contributions: Contributions;
-        profile: Profile;
-        submissionCalendar: string;
-        submitStats: SubmitStats;
-        badges: Badge[];
-        upcomingBadges: Badge[];
-        activeBadge: Badge | null;
+
+
+export interface QuesionsAnsweredResponse {
+    allQuestionsCount: AllQuestionsCount[]
+    matchedUser: {
+        submitStats: {
+            acSubmissionNum: {
+                difficulty: string
+                count: number
+                submissions: number
+            }[]
+        }
+        profile: {
+            ranking: number
+        }
     }
-        export interface Contributions {
-            points: number;
-            questionCount: number;
-            testcaseCount: number;
-        }
-        
-        export interface Profile {
-            realName: string;
-            websites: string[];
-            countryName: string | null;
-            skillTags: string[];
-            company: string | null;
-            school: string | null;
-            starRating: number;
-            aboutMe: string;
-            userAvatar: string;
-            reputation: number;
-            ranking: number;
-        }
-        
-        export interface SubmitStats {
-            acSubmissionNum: AcSubmissionNum[];
-            totalSubmissionNum: TotalSubmissionNum[];
-        }
-            export interface AcSubmissionNum {
-                difficulty: string;
-                count: number;
-                submissions: number;
-            }
-            export interface TotalSubmissionNum {
-                difficulty: string;
-                count: number;
-                submissions: number;
-            }
-        
-        export interface Badge {
+}
+export interface QUESTIONDATA {
+    ranking: number
+    all: number
+    hard: number
+    medium: number
+    easy: number
+    acceptance: number
+}
+
+
+
+
+export interface BadgeReponse {
+    matchedUser: {
+        badges: {
             id: string;
             displayName: string;
             icon: string;
-            creationDate?: string;
-        }
+            creationDate: string;
+        }[]
+    }
+}
+export interface BADGEDATA {
+    badges: {
+        displayName: string;
+        icon: string;
+        creationDate: string;
+    }[]
+}
 
 
-    export interface RecentSubmission {
+
+export interface RecentSubmissionResponse {
+    recentSubmissionList: {
         title: string;
         titleSlug: string;
         timestamp: string;
         statusDisplay: string;
         lang: string;
-    }
-export interface RecentSubmissionList {
-    [index: number]: RecentSubmission
-}
-export interface TopicTag {
-    name: string;
-    slug: string;
-    translatedName: string | null;
-}
-
-export interface CodeSnippet {
-    lang: string;
-    langSlug: string;
-    code: string;
-}
-
-export interface ChallengeQuestion {
-    id: string;
-    date: string;
-    incompleteChallengeCount: number;
-    streakCount: number;
-    type: string;
-}
-
-export interface DailyChallenge {
-    date: string;
-    link: string;
-    question: Problem;
-}
-
-    export type ProblemDifficulty = "Easy" | "Medium" | "Hard";
-    export interface Problem {
-        questionId: string;
-        questionFrontendId: string;
-        boundTopicId: unknown;
-        title: string;
-        titleSlug: string;
-        content: string;
-        translatedTitle: string | null;
-        translatedContent: string | null;
-        isPaidOnly: boolean;
-        difficulty: ProblemDifficulty;
-        likes: number;
-        dislikes: number;
-        isLiked: boolean | null;
-        similarQuestions: string;
-        exampleTestcases: string;
-        contributors: unknown[];
-        topicTags: TopicTag[];
-        companyTagStats: unknown;
-        codeSnippets: CodeSnippet[];
-        stats: string;
-        hints: string[];
-        solution: unknown;
-        status: unknown;
-        sampleTestCase: string;
-        metaData: string;
-        judgerAvailable: boolean;
-        judgeType: string;
-        mysqlSchemas: unknown[];
-        enableRunCode: boolean;
-        enableTestMode: boolean;
-        enableDebugger: boolean;
-        envInfo: string;
-        libraryUrl: string | null;
-        adminUrl: string | null;
-        challengeQuestion: ChallengeQuestion;
-        /** null if not logged in */
-        note: string | null;
-    }
-
-// Leetcode Response
-export enum LeetCodeGraphQLResponse {
-    UserProfile,
-    RecentSubmissionList,
-    DailyChallenge,
-    GraphQLError
+    }[]
 }
