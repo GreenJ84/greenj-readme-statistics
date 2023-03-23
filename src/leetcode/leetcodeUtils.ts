371bf765724ae7e473ea3b44e9964295fe5374bd
Jesse Greenough <jesse.greenough84@gmail.com>
2023-03-22 22:02:25 -0700

    Update typings for successfull stats card

--- src/leetcode/leetcodeUtils.ts
+++ src/leetcode/leetcodeUtils.ts
@@ -1,6 +1,7 @@
 import { match } from "ts-pattern";
-import { Request } from "express";
 import { badgesParse, questionsSolvedParse, recentQuestionsParse, statsParse } from "./apiParser";
+import { statsCard } from "./cards/stats-card";
+import { PROFILEDATA } from "./leetcodeTypes";
 
 // Returns the parse graph query file depending on path
 export const getGraph = (type: string): string => {
@@ -15,27 +16,92 @@ export const getGraph = (type: string): string => {
 }
 
 // Returns the parse creation function depending on path
-export const parseDirect = (req: Request): Function => {
-    const type = req.path.split("/")[2]!;
+export const parseDirect = (type: string): Function => {
     const parseFunc = match(type)
         .with("stats", () => {return statsParse})
         .with("badges", () => {return badgesParse})
         .with("questions_solved", () => {return questionsSolvedParse})
         .with("recent-questions", () => {return recentQuestionsParse})
-        // .with("daily-question", () => {return langsCardSetup})
+        // .with("daily-question", () => {return })
         .run()
     return parseFunc
 }
 
 // Returns the card creation function depending on path
-export const cardDirect = (req: Request): Function => {
-    const type = req.path.split("/")[2]!;
+export const cardDirect = (type: string): Function => {
     const parseFunc = match(type)
-        // .with("stats", () => {return statsCardSetup})
-        // .with("badges", () => {return trophCardSetup})
-        // .with("questions_solved", () => {return langsCardSetup})
-        // .with("recent-questions", () => {return langsCardSetup})
-        // .with("daily-question", () => {return langsCardSetup})
+        .with("stats", () => {return statsCard})
+        // .with("badges", () => {return })
+        // .with("questions_solved", () => {return })
+        // .with("recent-questions", () => {return })
+        // .with("daily-question", () => {return })
         .run()
     return parseFunc
 }
+
+// Maths......
+const normalcdf = (mean: number, sigma: number, to: number): number => {
+    var z = (to - mean) / Math.sqrt(2 * sigma * sigma);
+    var t = 1 / (1 + 0.3275911 * Math.abs(z));
+    var a1 = 0.254829592;
+    var a2 = -0.284496736;
+    var a3 = 1.421413741;
+    var a4 = -1.453152027;
+    var a5 = 1.061405429;
+    var erf =
+      1 - ((((a5 * t + a4) * t + a3) * t + a2) * t + a1) * t * Math.exp(-z * z);
+    var sign = 1;
+    if (z < 0) {
+        sign = -1;
+    }
+    return (1 / 2) * (1 + sign * erf);
+};
+
+// also Maths.... but that leads to words
+export const calculateRank = (stats: PROFILEDATA): string => {
+    const COMPLETION_OFFSET = 1.85;
+    const REP_OFFSET = 1;
+    const STARS_OFFSET = 1.25;
+    const BADGE_OFFSET = 1.5;
+    const CONTRIBS_OFFSET = 1.65;
+
+    const ALL_OFFSETS =
+        COMPLETION_OFFSET +
+        REP_OFFSET +
+        STARS_OFFSET + 
+        BADGE_OFFSET +
+        CONTRIBS_OFFSET;
+
+    const RANK_S_PLUS_VALUE = 1;
+    const RANK_S_VALUE = 25;
+    const RANK_A_VALUE = 45;
+    const RANK_A2_VALUE = 60;
+    const RANK_B_VALUE = 100;
+
+    const TOTAL_VALUES =
+        RANK_S_PLUS_VALUE +
+        RANK_S_VALUE +
+        RANK_A_VALUE +
+        RANK_A2_VALUE +
+        RANK_B_VALUE;
+
+    const score = (
+        parseFloat(stats.completion) * COMPLETION_OFFSET +
+        stats.reputation * REP_OFFSET +
+        stats.stars * STARS_OFFSET +
+        stats.badges * BADGE_OFFSET +
+        stats.contributions * CONTRIBS_OFFSET
+        ) / 100;
+
+    const normalizedScore = normalcdf(score, TOTAL_VALUES, ALL_OFFSETS) * 100;
+
+    const level = (() => {
+        if (normalizedScore < RANK_S_PLUS_VALUE) return "S+";
+        if (normalizedScore < RANK_S_VALUE) return "S";
+        if (normalizedScore < RANK_A_VALUE) return "A++";
+        if (normalizedScore < RANK_A2_VALUE) return "A+";
+        return "B+";
+    })();
+
+    return level;
+};
