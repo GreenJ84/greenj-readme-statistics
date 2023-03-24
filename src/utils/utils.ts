5ab2017ee0b361410ddc59d21b202ada86f31b48
Jesse Greenough <jesse.greenough84@gmail.com>
2023-03-23 23:48:48 -0700

    Implement global function to parse global card variables

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -1,4 +1,7 @@
 import { Request, Response } from "express";
+import { checkBlacklistRequest } from "./blacklist";
+import { THEMES, THEMETYPE } from "./themes";
+
 
 export const preFlight = (req: Request, res: Response): boolean => {
     if (req.params.username == undefined) {
@@ -7,12 +10,61 @@ export const preFlight = (req: Request, res: Response): boolean => {
                 message: 'No username found on API Call that requires username',
                 error: "Missing username parameter.",
                 error_code: 400
-        });
+            });
         return false
+    }
+    let accessCheck = checkBlacklistRequest(req, req.params.username!)
+    if (!accessCheck[0]) {
+        res.status(400).send(
+            {
+                message: accessCheck[1],
+                error: "Caller Black Listed",
+                error_code: 403
+            });
     }
     return true
 }
 
+export const baseCardThemeParse = (req: Request, _theme: THEMETYPE) => {
+    const {
+        theme,
+        background,
+        border,
+        stroke,
+
+        hideBorder,
+        borderRadius,
+        locale,
+    } = req.query;
+
+    // Set all properties base to theme first
+    if ((theme as string) in THEMES) {
+        _theme = THEMES[(theme as string)]!
+    } else {
+        _theme = THEMES["default"]!
+    }
+
+    if (background !== undefined) {
+        _theme.background = ("#" + background) as string;
+    }
+    if (border !== undefined) {
+        _theme.border = ("#" + border) as string;
+    }
+    if (stroke !== undefined) {
+        _theme.stroke = ("#" + stroke) as string;
+    }
+    if (hideBorder !== undefined) {
+        if (hideBorder == "true") {
+            _theme.hideBorder = true;
+        }
+    }
+    if (borderRadius !== undefined) {
+        _theme.borderRadius = parseInt(borderRadius as string);
+    }
+    if (locale !== undefined) {
+        _theme.locale = locale as string;
+    }
+}
 
 export function parse_cookie(cookie: string): Record<string, string> {
     return cookie
