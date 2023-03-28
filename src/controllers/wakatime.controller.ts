739b2e00be28c63f51020cc6434e3f7c83ec7e70
Jesse Greenough <jesse.greenough84@gmail.com>
2023-03-27 21:34:03 -0700

    Implment parsing and card creation fucntionality

--- src/controllers/wakatime.controller.ts
+++ src/controllers/wakatime.controller.ts
@@ -1,12 +1,23 @@
 import { Request, Response } from 'express';
+import { preFlight } from '../utils/utils';
+import { parseDirect } from '../wakatime/apiParse';
+import { getUserStats } from '../wakatime/query';
+import { wakaResponse } from '../wakatime/wakatimeTypes';
+import { cardDirect } from '../wakatime/wakatimeUtils';
 
-export const getProfileStats = async (req: Request, res: Response) => {
-    const type = req.path.split('/')[2]!;
+export const getProfileStats = async (req: Request, res: Response): Promise<void> => {
+    if (!preFlight(req, res)) {
+        return;
+    }
+    const data: wakaResponse = await getUserStats(req);
 
-    // query user data or access chache
-
+    const type = req.path.split("/")[2]!;
+    const dataParse = parseDirect(type);
+    const cardCreate = cardDirect(type);
     // parse needed data properly
+    const parsedData = dataParse(data);
 
     // create card from parsed data
-    // return card
+    const card = cardCreate(parsedData);
+    res.status(200).send(card);
 }
