d33ecd75c50234c5b2be34b63eeff6e8f66dae76
Jesse Greenough <jesse.greenough84@gmail.com>
2023-03-27 21:32:48 -0700

    Update query rendering and flow

--- src/wakatime/query.ts
+++ src/wakatime/query.ts
@@ -1,24 +1,30 @@
-import axios, { AxiosRequestConfig } from 'axios';
+import axios from 'axios';
 import dotenv from 'dotenv';
-import { WAKA_TIME_AUTH_URL, WAKA_TIME_URL } from '../utils/constants';
+import { Request } from 'express';
+import { WAKA_TIME_URL } from '../utils/constants';
+import { wakaResponse } from './wakatimeTypes';
 
 dotenv.config()
 
-const setAuthentication = () => {
+export const getUserStats = async (req: Request): Promise<wakaResponse> => {
+    const { username } = req.params;
 
-}
-
-const authenticate = () => {
-    
-}
-
-export const getUserStats = async (): Promise<any> => {
     const config = axios.create({
-        baseURL: WAKA_TIME_AUTH_URL,
+        baseURL: WAKA_TIME_URL,
         headers: {
             Authorization: `Basic ${Buffer.from(process.env.WAKATIME_TOKEN!).toString('base64')}`
         }
     });
-
+    const data = await config.get(`users/${username}/stats/all_time`, {
+        params: {}
+    })
+        .then(res => { return res.data })
+        .catch(err => { return {
+            message: "Error accessing WakaTime API",
+            error: err,
+            error_code: 500
+        } })
     
+    console.log(data);
+    return data;
 }
