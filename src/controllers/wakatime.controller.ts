4dcff9e00fe10abf406c11439bc94c6cfdd5443b
Jesse Greenough <jesse.greenough84@gmail.com>
2023-03-29 23:24:42 -0700

    Remove redis caching for now

--- src/controllers/wakatime.controller.ts
+++ src/controllers/wakatime.controller.ts
@@ -1,7 +1,6 @@
 import { Request, Response } from 'express';
 
 import { preFlight } from '../utils/utils';
-import { getCacheData, setCacheData } from '../utils/cache';
 import { Error } from '../utils/constants';
 
 import { getUserStats } from '../wakatime/query';
@@ -15,9 +14,6 @@ export const getProfileStats = async (req: Request, res: Response): Promise<void
         return;
     }
 
-    // Get direct-uncached API request param if present 
-    const { refresh } = req.query;
-
     // Get API Route subtype
     const type = req.path.split("/")[2]!;
 
@@ -25,27 +21,11 @@ export const getProfileStats = async (req: Request, res: Response): Promise<void
     const dataParse = parseDirect(type);
     const cardCreate = cardDirect(type);
 
-    // Check cache if not a direct request
-    if (refresh !== "true") {
-        // Check cache for entry
-        const cachedData = await getCacheData(req);
-        // Create based of cache if present
-        if (cachedData[0]) {
-            // parse cached data
-            const parsedData = dataParse(cachedData[1]);
-            // create card from parsed data
-            const card = cardCreate(parsedData);
-            res.status(200).send(card);
-            return;
-        }
-    }
-
     const data: wakaResponse | Error = await getUserStats(req);
     if ((data as Error).error !== undefined) {
         res.status((data as Error).error_code).send(data);
         return;
     } 
-    setCacheData(req, data)
     // parse needed data properly
     const parsedData = dataParse(data);
 
