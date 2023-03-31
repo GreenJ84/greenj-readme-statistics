a3c908dddd5d2515cc17d869c5c1a663c3a059aa
Jesse Greenough <jesse.greenough84@gmail.com>
2023-03-31 11:57:43 -0700

    Update, clean, and organize LeetCode controller
    Update applied LeetCode routes

--- src/controllers/leetcode.controller.ts
+++ src/controllers/leetcode.controller.ts
@@ -2,44 +2,43 @@
 import fs from 'fs';
 import { Request, Response } from "express";
 import gql from "graphql-tag";
+
+import { ResponseError, GRAPHQL_URL } from "../utils/constants";
+import { STREAKDATA } from "../leetcode/leetcodeTypes";
+
+import { THEMES } from '../utils/themes';
+import { preFlight } from "../utils/utils";
 import { parseDirect } from "../leetcode/apiParser";
-import { LeetCodeGraphQLResponse, STREAKDATA } from "../leetcode/leetcodeTypes";
 import {cardDirect, getGraph } from "../leetcode/leetcodeUtils";
 import { leetcodeGraphQL, preProbe, preQuery } from '../leetcode/query';
-import { GraphQLError, GRAPHQL_URL } from "../utils/constants";
-import { preFlight } from "../utils/utils";
-import { THEMES } from '../utils/themes';
-
-// PreHandler for routes requiring username
-export const leetcodeProfile = async (req: Request, res: Response): Promise<void> => {
-    if (!preFlight(req, res)) {
-        res.status(400).send({
-            message: "There was no username provided for a route that requires it. Provide a valid username before calling again.",
-            error: { "Missing Parameter": "Username" },
-            error_code: 400
-        });
-        return;
-    }
-    leetcodeStats(req, res);
-    return;
-};
 
 // Main Controller for GitHub
 export const leetcodeStats = async (req: Request, res: Response): Promise<void> => {
+    // Get route used
     const type = req.path.split("/")[2]!;
+    // PreFlight checks for user based routes
+    if (type !== "daily-question" && !preFlight(req, res)) {
+        return;
+    }
+    // Silo of streak to its own controller
     if (type == "streak") {
         leetcodeStreak(req, res);
         return
     }
-    const data: LeetCodeGraphQLResponse = await preQuery(req, res, type)
-        .then((data: LeetCodeGraphQLResponse | GraphQLError) => {
-            return data as LeetCodeGraphQLResponse;
-        });
-    
     const parse = parseDirect(type);
+    const createCard = cardDirect(type);
+
+
+    const data = await preQuery(req, res, type)
+    if ((data as ResponseError).error !== undefined) {
+        console.error((data as ResponseError).message)
+        console.error((data as ResponseError).error)
+        res.status((data as ResponseError).error_code).send(data);
+        return;
+    } 
+    
     const parsedData = parse(data);
 
-    const createCard = cardDirect(type);
     const card = createCard(req, parsedData);
 
     res.status(200).send(card);
@@ -47,16 +46,17 @@ export const leetcodeStats = async (req: Request, res: Response): Promise<void> 
 }
 
 // User Streak specific controller
-export const leetcodeStreak = async (req: Request, res: Response): Promise<void> => {
+const leetcodeStreak = async (req: Request, res: Response): Promise<void> => {
     const parseStreak = parseDirect("streak");
     const streakCard = cardDirect("streak")
+    const path = getGraph("streak");
     
-    const response = await preProbe(req, res);
-    if (response == false) {
+    const preSet = await preProbe(req, res);
+    if (preSet == false) {
         return;
     }
-    console.log("Streak open----")
-    const [membershipYears, csrf_credential] = response as [number[], string];
+
+    const [membershipYears, csrf_credential] = preSet as [number[], string];
     const { username } = req.params;
     const streakData: STREAKDATA = {
         streak: [0, 0],
@@ -66,17 +66,18 @@ export const leetcodeStreak = async (req: Request, res: Response): Promise<void>
         completionActuals: [0,0],
         theme: THEMES["black-ice"]!
     }
-    const path = getGraph("streak");
+
     const graphql = gql(
         fs.readFileSync(path, 'utf8')
     );
+
+    // Call the universal leetCode querier for each year
     for (let year of membershipYears) {
-        console.log("Streak call----")
-        // Call the universal leetCode querier
+
         const data = await leetcodeGraphQL({
-                query: graphql,
-                variables: { username: username!, year: year }
-            },
+            query: graphql,
+            variables: { username: username!, year: year }
+        },
             GRAPHQL_URL,
             csrf_credential)
             .then((res) => res)
@@ -85,12 +86,13 @@ export const leetcodeStreak = async (req: Request, res: Response): Promise<void>
                     message: "Internal server error",
                     error: err,
                     error_code: 500,
-                } as GraphQLError;
-            })
-        
+                } as ResponseError;
+            });
+
         parseStreak(streakData, data, year);
     }
 
-    const card = streakCard(req, streakData)
-    res.status(200).send(card)
+    const card = streakCard(req, streakData);
+
+    res.status(200).send(card);
 }
--- src/routes/leetcode.routes.ts
+++ src/routes/leetcode.routes.ts
@@ -1,30 +1,30 @@
 import express from "express";
 
-import * as LeetcodeController from "../controllers/leetcode.controller";
+import {leetcodeStats} from "../controllers/leetcode.controller";
 
 export const LeetCodeRoutes = (app: express.Application) => {
     // Stars, contribution points, reputaion, rating
     app.get('/leetcode/stats/:username',
-        LeetcodeController.leetcodeProfile
+        leetcodeStats
     )
     // Display LeetCode streak and activity
     app.get('/leetcode/streak/:username',
-    LeetcodeController.leetcodeStreak
+        leetcodeStats
     )
     // Display earned leetcode badges
     app.get('/leetcode/badges/:username',
-        LeetcodeController.leetcodeProfile
+        leetcodeStats
     )
     // Get number of questions solved in total and by level
     app.get('/leetcode/questions_solved/:username',
-        LeetcodeController.leetcodeProfile
+        leetcodeStats
     )
     // Display top 5 most recent questions solved and their languages
     app.get('/leetcode/recent-questions/:username',
-        LeetcodeController.leetcodeProfile
+        leetcodeStats
     )
     // Get a random leetcode problem title
     app.get('/leetcode/daily-question',
-        LeetcodeController.leetcodeStats
+        leetcodeStats
     )
 }
