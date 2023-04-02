819d481e110deed68428d87322110d918412e20a
Jesse Greenough <jesse.greenough84@gmail.com>
2023-04-01 18:03:57 -0700

    Create date formating functionality
    Create parameter normalization functionality

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -1,4 +1,7 @@
+import { DateTime } from 'luxon';
 import { Request, Response } from "express";
+import { createIntl, createIntlCache } from '@formatjs/intl';
+
 import { checkBlacklistRequest } from "./blacklist";
 import { THEMES, THEMETYPE } from "./themes";
 
@@ -11,7 +14,7 @@ export const preFlight = (req: Request, res: Response): boolean => {
                 error: "Missing username parameter.",
                 error_code: 400
             });
-        return false
+        return false;
     }
     let accessCheck = checkBlacklistRequest(req, req.params.username!)
     if (!accessCheck[0]) {
@@ -21,8 +24,19 @@ export const preFlight = (req: Request, res: Response): boolean => {
                 error: "Caller Black Listed",
                 error_code: 403
             });
+        return false;
     }
-    return true
+    return true;
+}
+
+/**
+ *  Normalize a param name
+ *  @param param param name
+ *  @return Normalized param name
+ */
+function normalizeParam(param: string): string {
+    // Convert to lower and ensure kebab case
+    return param.toLowerCase().replace("_", "-");
 }
 
 export const baseCardThemeParse = (req: Request, _theme: THEMETYPE) => {
@@ -38,8 +52,13 @@ export const baseCardThemeParse = (req: Request, _theme: THEMETYPE) => {
     } = req.query;
 
     // Set all properties base to theme first
-    if ((theme as string) in THEMES) {
-        _theme = THEMES[(theme as string)]!
+    if (theme != undefined) {
+        const normTheme = normalizeParam(theme as string)
+        if ((normTheme as string) in THEMES) {
+            _theme = THEMES[normTheme]!
+        } else {
+            _theme = THEMES["default"]!
+        }
     } else {
         _theme = THEMES["default"]!
     }
@@ -69,7 +88,7 @@ export const baseCardThemeParse = (req: Request, _theme: THEMETYPE) => {
     }
 
     if (locale !== undefined) {
-        _theme.locale = locale as string;
+        _theme.locale = normalizeParam(locale as string);
     } else {
         _theme.locale = 'en-US';
     }
@@ -93,3 +112,57 @@ export function sleep(ms: number, val: unknown = null): Promise<unknown> {
     return new Promise((resolve) => setTimeout(() => resolve(val), ms));
 }
 
+
+/**
+    Convert date from Y-M-D to more human-readable format
+ *  @param string $dateString String in Y-M-D format
+    *  @param string $locale Locale code
+ *  @param string|null $format Date format to use, or null to use locale default
+ *  @return string Formatted Date string
+ */
+export const getFormatDate = (dateString: string, locale: string, format: string | null = null): string => {
+    const date = DateTime.fromISO(dateString);
+    let formatted = '';
+    const cache = createIntlCache()
+    const dateConfig = createIntl(
+        {
+        locale: locale,
+        timeZone: "UTC",
+        defaultLocale: 'en-US',
+        messages: {},
+        formats: {},
+        defaultFormats: {},
+        onError: (err) => {console.error(err) },
+        onWarn: (warning) => { console.warn(warning)}
+        },
+        cache
+    )
+    // if current year, display only month and day
+    if (date.toFormat('yyyy') === DateTime.now().toFormat('yyyy')) {
+        if (format) {
+        // remove brackets and all text within them
+        formatted = date.toFormat(format.replace(/\[.*?\]/g, ''));
+        } else {
+        // format without year using locale
+        formatted = dateConfig.formatDate(date.toISODate(), { month: 'short', day: 'numeric' });
+        }
+    }
+    // otherwise, display month, day, and year
+    else {
+        if (format) {
+        // remove brackets, but leave text within them
+        formatted = date.toFormat(format.replace(/[\[\]]/g, ''));
+        } else {
+        // format with year using locale
+        formatted = dateConfig.formatDate(date.toISODate(), { year: 'numeric', month: 'short', day: 'numeric' });
+        }
+    }
+    // sanitize and return formatted date
+    return formatted.replace(/[<>"&]/g, (match) => {
+        if (match == '<') { return '&lt;' }
+        else if (match == '>') { return '&gt;' }
+        else if (match == '"') { return '&quot;' }
+        else if (match == '&') { return '&amp;' }
+        else return ""
+    });
+}
