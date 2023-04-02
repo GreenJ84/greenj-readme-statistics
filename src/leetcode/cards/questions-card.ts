753e0267f5fe1bacaade2ef7a8c2f733aaa91a89
Jesse Greenough <jesse.greenough84@gmail.com>
2023-04-02 15:43:12 -0700

    Finalize leetcode questions card

--- src/leetcode/cards/questions-card.ts
+++ src/leetcode/cards/questions-card.ts
@@ -57,92 +57,93 @@ export const questionsCard = (req: Request, data: QUESTIONDATA): string => {
             text-align: center;
             letter-spacing: 1.5px;
         }
-    
-    .stagger {
-        opacity: 0;
-        animation: fadeInAnimation 0.3s ease-in-out forwards;
-    }
-    
-    .acceptance-text {
-        font: 800 24px 'Segoe UI', Ubuntu, Sans-Serif;
-        fill: ${theme.statsMain};
-        animation: scaleInAnimation 0.3s ease-in-out forwards;
-    }
-    
-    .acceptance-title {
-        font: 600 14px 'Segoe UI', Ubuntu, "Helvetica Neue", Sans-Serif;
-        fill: ${theme.textMain};
-        animation: scaleInAnimation 0.3s ease-in-out forwards;
-    }
+        .stagger {
+            opacity: 0;
+            animation: fadeInAnimation 0.3s ease-in-out forwards;
+        }
+        
+        .acceptance-text {
+            font: 800 24px 'Segoe UI', Ubuntu, Sans-Serif;
+            fill: ${theme.statsMain};
+            animation: scaleInAnimation 0.3s ease-in-out forwards;
+        }
+        
+        .acceptance-title {
+            font: 600 14px 'Segoe UI', Ubuntu, "Helvetica Neue", Sans-Serif;
+            fill: ${theme.textMain};
+            animation: scaleInAnimation 0.3s ease-in-out forwards;
+        }
 
-    .bold {
-        font-weight: 700
-    }
-    
-    .acceptance-circle-rim {
-        stroke: ${theme.detailMain};
-        fill: none;
-        stroke-width: 6;
-        opacity: 0.2;
-    }
-    
-    .acceptance-circle {
-        stroke: ${theme.detailMain};
-        stroke-dasharray: 400;
-        fill: none;
-        stroke-width: 6;
-        stroke-linecap: round;
-        opacity: 0.8;
-        transform-origin: -10px 8px;
-        transform: rotate(-90deg);
-        animation: acceptanceAnimation 1s forwards ease-in-out;
-    }
-    
-    @keyframes acceptanceAnimation {
-        from {
-        stroke-dashoffset: 400;
+        .bold {
+            font-weight: 700
         }
-        to {
-        stroke-dashoffset: ${400-parseInt(data.acceptance)*4};
+        
+        .acceptance-circle-rim {
+            stroke: ${theme.detailMain};
+            fill: none;
+            stroke-width: 6;
+            opacity: 0.2;
         }
-    }
-    /* Animations */
-    
-    @keyframes scaleInAnimation {
-        from {
-        transform: translate(-5px, 5px) scale(0);
+        
+        .acceptance-circle {
+            stroke: ${theme.detailMain};
+            stroke-dasharray: calc(${parseInt(data.acceptance)} * (377 / 100)) 377;
+            fill: none;
+            stroke-width: 6;
+            stroke-linecap: round;
+            transform-origin: -10px 8px;
+            animation: acceptanceAnimation 1s forwards ease-in-out;
         }
-        to {
-        transform: translate(-5px, 5px) scale(1);
+        
+        /* Animations */
+        @keyframes acceptanceAnimation {
+            from {
+                stroke-dashoffset: -200;
+                transform: rotate(90deg);
+            }
+            to {
+                stroke-dashoffset: 0;
+                transform: rotate(-90deg);
+            }
         }
-    }
-    
-    @keyframes fadeInAnimation {
-        from {
-        opacity: 0;
+        @keyframes scaleInAnimation {
+            from {
+            transform: translate(-5px, 5px) scale(0);
+            }
+            to {
+            transform: translate(-5px, 5px) scale(1);
+            }
         }
-        to {
-        opacity: 1;
+        
+        @keyframes fadeInAnimation {
+            from {
+            opacity: 0;
+            }
+            to {
+            opacity: 1;
+            }
         }
-    }
     </style>
     <defs>
         <clipPath id='outer_rectangle'>
             <rect width='552' height='215' rx='${theme.borderRadius}'/>
         </clipPath>
     </defs>
+
+    <!-- Background -->
     <g style='isolation: isolate'>
             <rect stroke='${ theme.hideBorder ? theme.background : theme.border}' fill='${theme.background}' rx='${theme.borderRadius}' x="1.5" y="1.5" stroke-width="2" width="549" height="212"/>
     </g>
 
     <!-- Title -->
     <g transform="translate(90,0)">
-        <text x="120.5" y="28" stroke-width="0" text-anchor="middle" fill="${theme.textMain}" stroke="none" font-family="\'Segoe UI\', Ubuntu, sans-serif" font-weight="400" font-size="24px" font-style="normal" style="opacity: 0; animation: fadein 0.5s linear forwards 0.7s; letter-spacing: 4px; text-shadow: 1px 1px 2px black;">
+        <text x="120.5" y="28" stroke-width="0" text-anchor="middle" fill="${theme.textMain}" stroke="none" font-family="\'Segoe UI\', Ubuntu, sans-serif" font-weight="400" font-size="24px" font-style="normal" style="opacity: 0; animation: fadeInAnimation 0.5s linear forwards 0.7s; letter-spacing: 4px; text-shadow: 1px 1px 2px black;">
             ${data.title}
         </text>
     </g>
 
     <g transform="translate(0, 55)">
+    <!-- Acceptance -->
         <g transform="translate(460, 47.5)">
             <circle class="acceptance-circle-rim" cx="-10" cy="8" r="60" />
             <circle class="acceptance-circle" cx="-10" cy="8" r="60" />
@@ -153,13 +154,15 @@ export const questionsCard = (req: Request, data: QUESTIONDATA): string => {
                 acceptance 
             </text>
         </g>
+
+    <!-- Ranking -->
         <g transform="translate(0, 0)">
             <g class="stagger" style="animation-delay: 450ms" transform="translate(25, 0)" >
             <text class="stat bold" y="12.5">
                 Ranking:
             </text>
             <text class="num" text-anchor="middle" x="260" y="12.5">
-                ${data.ranking}
+                ${data.ranking.toLocaleString()}
             </text>
             </g>
         </g>
@@ -169,17 +172,18 @@ export const questionsCard = (req: Request, data: QUESTIONDATA): string => {
                 Total Questions Solved:
             </text>
             <text class="all num" text-anchor="middle" x="235" y="12.5">
-                ${data.all[0]}
+                ${data.all[0].toLocaleString()}
             </text>
             <text class="all stat" text-anchor="middle" x="260" y="12.5">
                 /
             </text>
             <text class="all stat" text-anchor="middle" x="285" y="12.5">
-                ${data.all[1]}
+                ${data.all[1].toLocaleString()}
             </text>
             </g>
         </g>
 
+    <!-- Easy Questions -->
         <g transform="translate(0, 60)">
             <g class="stagger" style="animation-delay: 750ms" transform="translate(25, 0)">
             <text class="stat bold easy" y="12.5">
@@ -197,23 +201,25 @@ export const questionsCard = (req: Request, data: QUESTIONDATA): string => {
             </g>
         </g>
 
+    <!-- Medium questions -->
         <g transform="translate(0, 90)">
             <g class="stagger" style="animation-delay: 900ms" transform="translate(25, 0)">
             <text class="stat bold medium" y="12.5">
                 Medium Questions Solved:
             </text>
             <text class="med num" text-anchor="middle" x="235" y="12.5">
-                ${data.medium[0]}
+                ${data.medium[0].toLocaleString()}
             </text>
             <text class="med stat" text-anchor="middle" x="260" y="12.5">
                 /
             </text>
             <text class="med stat" text-anchor="middle" x="285" y="12.5">
-                ${data.medium[1]}
+                ${data.medium[1].toLocaleString()}
             </text>
             </g>
         </g>
 
+    <!-- Hard Questions -->
         <g transform="translate(0, 120)">
             <g class="stagger" style="animation-delay: 1050ms" transform="translate(25, 0)">
             <text class="stat bold hard" y="12.5">
