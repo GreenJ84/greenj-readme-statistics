0ff28e62305cb4cad23dbfc70689bd7ba9ab1639
Jesse Greenough <jesse.greenough84@gmail.com>
2023-04-02 19:18:23 -0700

    Create development connection for Redis caching

--- src/utils/cache.ts
+++ src/utils/cache.ts
@@ -1,30 +1,36 @@
-import { Request } from 'express';
-import redis from 'redis';
-const client = redis.createClient();
+import { RedisClientType, createClient } from 'redis';
+const client: RedisClientType = process.env.NODE_ENV === "production" ?
+    createClient({
+            url: `redis://${process.env.REDIS_USER!}:${process.env.REDIS_PASS!}@${process.env.PROD_HOST!}:${parseInt(process.env.PROD_PORT!)}`
+        })
+    :
+    createClient();
 
 client.on("error", err => console.error(`Redis client error: ${err}`))
 
 client.connect()
     .then(() => console.log("Redis connection created."))
 
-export const getCacheData = async (req: Request): Promise<[boolean, object?]> => {
+export const getCacheData = async (key: string): Promise<[boolean, object | null]> => {
+    console.log("Getting cache");
     try {
         const data = await client.get(
-            `${req.path.split("/")[1]!}:${req.params.username!}`
+            key
         );
         if (data == undefined) {
-            return [false]
+            return [false, null]
         }
         return [true, JSON.parse(data)]
     }
     catch {
-        return [false]
+        return [false, null]
     }
 }
 
-export const setCacheData = async (req: Request, data: any): Promise<void> => {
+export const setCacheData = async (key: string, data: any): Promise<void> => {
+    console.log("Setting cache");
     await client.set(
-        `${req.path.split("/")[1]!}:${req.params.username!}`,
+        key,
         JSON.stringify(data), {
             "EX": (60 * 60 * 6)
         }
