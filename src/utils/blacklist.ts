36613dae68206568b43a377ff3324814b28a9863
Jesse Greenough <jesse.greenough84@gmail.com>
2023-04-03 01:43:04 -0700

    Update blacklist functionality and add filled blacklisted

--- src/utils/blacklist.ts
+++ src/utils/blacklist.ts
@@ -11,10 +11,10 @@ export const addToBlacklist = (newBlacklist: string) => {
 };
 
 export const checkBlacklistRequest = (req: Request, user: string = "DONTEVERBLOCKME"): [boolean, string] => {
-    if (blacklist.includes(req.headers['user-agent']!)) {
+    if (req.headers !== undefined && 'user-agent' in req.headers && blacklist.includes(req.headers['user-agent']!)) {
         return [false, "I do not like bots that cannot be nice. You have been excluded. 01000110 01110101 01100011 01101011 00100000 01101111 01100110 01100110 00100000 01100010 01100001 01100100 00100000 01100010 01101111 01110100"];
     }
-    else if (blacklist.includes(req.ip)) {
+    else if (req.ip !== undefined && blacklist.includes(req.ip)) {
         return [false, "This specific IP has lost access to this api. Feel free to create your own server with the code."];
     }
     else if (blacklist.includes(user)) {
