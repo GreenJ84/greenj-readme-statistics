c56f1c30fb229d3f9f53c865f95b94c9bf09436c
Jesse Greenough <jesse.greenough84@gmail.com>
2023-04-04 23:08:55 -0700

    Update error handling within credentials request

--- src/utils/credentials.ts
+++ src/utils/credentials.ts
@@ -1,4 +1,6 @@
-import { CRED_URL, GRAPHQL_URL_CN, USER_AGENT } from "../utils/constants";
+import axios from 'axios';
+
+import { CRED_URL, ResponseError, USER_AGENT } from "../utils/constants";
 import { parse_cookie } from "./utils";
 
 // Credential
@@ -9,41 +11,48 @@ export interface ICredential {
     csrf?: string;
 }
 
-export async function get_csrf(): Promise<String> {
-    const cookies_raw = await fetch(CRED_URL, {
+export async function get_csrf(): Promise<string | ResponseError> {
+    const cookies_raw = await axios.get(CRED_URL, {
         headers: {
             "user-agent": USER_AGENT,
         },
     }).then((res) => {
-        return res.headers.get("set-cookie") as string;
+        return res.headers['set-cookie']![0] as string;
     })
     .catch((err) => {
-        return `error: ${err}`
+        return {
+            message: "LeetCode credentials API response error",
+            error: err,
+            error_code: 502,
+        } as ResponseError;
     });
-    const csrf_token = parse_cookie(cookies_raw).csrftoken!;
+    if ((cookies_raw as ResponseError).error !== undefined) {
+        return cookies_raw;
+    }
+    const csrf_token = parse_cookie(cookies_raw as string).csrftoken!;
 
     return csrf_token;
 };
 
-export async function get_csrf_cn() {
-    const cookies_raw = await fetch(GRAPHQL_URL_CN, {
-        method: "POST",
-        headers: {
-            "content-type": "application/json",
-            "user-agent": USER_AGENT,
-        },
-        body: JSON.stringify({
-            operationName: "nojGlobalData",
-            variables: {},
-            query: "query nojGlobalData {\n  siteRegion\n  chinaHost\n  websocketUrl\n}\n",
-        }),
-    }).then((res) => {
-        return res.headers.get("set-cookie") as string;
-    }).catch((err) => {
-        return `error: ${err}`
-    });
+// export async function get_csrf_cn() {
+//     const cookies_raw = await fetch(GRAPHQL_URL_CN, {
+//         method: "POST",
+//         headers: {
+//             "content-type": "application/json",
+//             "user-agent": USER_AGENT,
+//         },
+//         body: JSON.stringify({
+//             operationName: "nojGlobalData",
+//             variables: {},
+//             query: "query nojGlobalData {\n  siteRegion\n  chinaHost\n  websocketUrl\n}\n",
+//         }),
+//     }).then((res) => {
+//         return res.headers['set-cookie']![0] as string;
+//     }).catch((err) => {
+//         return `error: ${err}`
+//     });
 
-    const csrf_token = parse_cookie(cookies_raw).csrftoken;
+//     const csrf_token = parse_cookie(cookies_raw).csrftoken;
 
-    return csrf_token;
-};
+//     return csrf_token;
+// };
