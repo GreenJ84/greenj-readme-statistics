265786df050f4051dd27e253c0badfe2c3961e56
Jesse Greenough <jesse.greenough84@gmail.com>
2023-04-07 07:27:51 -0700

    Update CSP policies

--- src/server.ts
+++ src/server.ts
@@ -79,8 +79,8 @@ app.use(
       directives: {
         defaultSrc: ["'self'"],
         scriptSrc: ["'self'"],
-        styleSrc: ["'self'", "unsafe-inline"],
-        "img-src": ["'self'", "data:", "https://greenj-readme-stats.onrender.com"]
+        styleSrc: ["'self'", "'unsafe-inline'"],
+        "img-src": ["'self'", "data:", "https://greenj-readme-stats.onrender.com", "http://localhost:8000"]
       },
     },
     xssFilter: true,
