a5ea793100272b3ff05ed9c86c6a319ab3fb77f4
Jesse Greenough <jesse.greenough84@gmail.com>
2023-04-09 15:35:30 -0700

    Update error handling on all wakatime routes

--- src/routes/wakatime.routes.ts
+++ src/routes/wakatime.routes.ts
@@ -1,18 +1,30 @@
-import express from "express";
+import express, {Request, Response, NextFunction} from "express";
 
-import * as wakatime from '../controllers/wakatime.controller';
+import {getProfileStats} from '../controllers/wakatime.controller';
 
 export const WakaTimeRoutes = (app: express.Application) => {
     // coding/debugging, top project, top lang, top os, top editor, top machine
     app.get('/wakatime/insights/:username',
-        wakatime.getProfileStats
-    );
+        (req: Request, res: Response, next: NextFunction) => {
+            getProfileStats(req, res)
+            .catch(err => {
+                next(err)
+            })
+    });
     // Languages used wheel
     app.get('/wakatime/languages/:username',
-        wakatime.getProfileStats
-    );
+        (req: Request, res: Response, next: NextFunction) => {
+            getProfileStats(req, res)
+            .catch(err => {
+                next(err)
+            })
+    });
     // , best day, total days
     app.get('/wakatime/stats/:username',
-        wakatime.getProfileStats
-    );
+        (req: Request, res: Response, next: NextFunction) => {
+            getProfileStats(req, res)
+            .catch(err => {
+                next(err)
+            })
+    });
 }
