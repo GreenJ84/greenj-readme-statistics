5658e51455e0e6619a77f14cc2ee24a7926cf095
Jesse Greenough <jesse.greenough84@gmail.com>
2023-04-09 15:35:17 -0700

    Update error handling on all github routes

--- src/routes/github.routes.ts
+++ src/routes/github.routes.ts
@@ -1,22 +1,38 @@
-import express from "express";
+import express, {Request, Response, NextFunction} from "express";
 
-import * as Github from '../controllers/github.controller';
+import {getCommitStreak, getProfileStats} from '../controllers/github.controller';
 
 export const GithubRoutes = (app: express.Application) => {
     // Display github stats in trophies
     app.get('/github/trophies/:username',
-        Github.getProfileStats
-    )
+        (req: Request, res: Response, next: NextFunction) => {
+        getProfileStats(req, res)
+        .catch(err => {
+            next(err)
+        })
+    });
     // Display github stats in Modal
-    app.get('/github/stats/:username',
-        Github.getProfileStats
-    )
+app.get('/github/stats/:username',
+    (req: Request, res: Response, next: NextFunction) => {
+        getProfileStats(req, res)
+        .catch(err => {
+            next(err)
+        })
+    });
     // Display most used github languages
-    app.get('/github/languages/:username',
-        Github.getProfileStats
-    )
+app.get('/github/languages/:username',
+    (req: Request, res: Response, next: NextFunction) => {
+        getProfileStats(req, res)
+        .catch(err => {
+            next(err)
+        })
+    });
     // Display github contributions streak
-    app.get('/github/streak/:username',
-        Github.getCommitStreak
-    )
+app.get('/github/streak/:username',
+    (req: Request, res: Response, next: NextFunction) => {
+        getCommitStreak(req, res)
+        .catch(err => {
+            next(err)
+        })
+    });
 }
