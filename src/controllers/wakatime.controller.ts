141c94f8e04ed4912a364526eaaa5268ba3ff923
Jesse Greenough <jesse.greenough84@gmail.com>
2023-04-12 12:07:48 -0700

    Remove data querying form wakatime route
    Implement wakatime registration route to inisiate initial data query and adds an interval for resreshing data in cache
    
    Streamline main routes to handle cache retrieval and building

--- src/controllers/wakatime.controller.ts
+++ src/controllers/wakatime.controller.ts
@@ -3,13 +3,54 @@ import { Request, Response } from 'express';
 import { preFlight, sleep } from '../utils/utils';
 // import { ResponseError } from '../utils/constants';
 
-import { getUserStats } from '../wakatime/query';
+import { getUserStats, updateUser } from '../wakatime/query';
 import { parseDirect } from '../wakatime/apiParse';
 import { cardDirect } from '../wakatime/wakatimeUtils';
 import { wakaResponse } from '../wakatime/wakatimeTypes';
 import { getCacheData, setCacheData } from '../utils/cache';
+import { PRODUCTION } from '../utils/constants';
 
 let sleepMod = -2;
+
+// 30 sec interval for development
+// 8hr interval for production
+const DATA_UDPDATE_INTERVAL = PRODUCTION ? 1000 * 60 * 60 * 8 : 1000 * 30;
+
+export const wakaStatsRegister = async (req: Request, res: Response): Promise<void> => {
+    // Ensure caller is viable
+    if (!preFlight(req, res)) {
+        return;
+    }
+    const cacheKey = `wakatime:${req.params.username!}`;
+    res.set('Content-Type', 'application/json');
+    
+    // Try for cached data, Query API if not present
+    const [success, _] = await getCacheData(cacheKey);
+    if (success) { 
+        res.status(208).json({
+            message: "User already registered",
+            code: "208"
+        })
+    }
+    // Query WakaTime api
+    const queryRepsonse: wakaResponse = await getUserStats(req.params.username!)
+        .catch(err => {
+            throw err;
+        });
+    // Add new query data to cache
+    await setCacheData(cacheKey, queryRepsonse);
+
+    setInterval(() => {
+        console.log("Updating");
+        updateUser(cacheKey, req.params.username!);
+    }, DATA_UDPDATE_INTERVAL);
+    
+    res.status(201).json({
+        message: "User Registered",
+        code: "201"
+    });
+}
+
 
 export const getProfileStats = async (req: Request, res: Response): Promise<void> => {
     // Ensure caller is viable
@@ -28,15 +69,12 @@ export const getProfileStats = async (req: Request, res: Response): Promise<void
     let data: wakaResponse;
     const [success, cacheData] = await getCacheData(cacheKey);
     if (!success) {
-        // Query WakaTime api
-        const queryRepsonse: wakaResponse = await getUserStats(req)
-            .catch(err => {
-                throw err;
-            });
-            // Add new query data to cache
-            setCacheData(cacheKey, queryRepsonse)
-            
-            data = queryRepsonse;
+        res.set('Content-Type', 'application/json');
+        res.status(401).json({
+            message: "User unauthorized. Registration required for API data.",
+            code: "401"
+        });
+        return;
     }
     else {
         data = cacheData as wakaResponse;
