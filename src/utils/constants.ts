1d6b9429531d98543416b7bb00c7eb20c75a209b
Jesse Greenough <jesse.greenough84@gmail.com>
2023-04-12 12:05:59 -0700

    Implement env variables in constants file

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,4 +1,14 @@
 import { DocumentNode } from "graphql";
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+// Environment Variables
+export const PRODUCTION = process.env.NODE_ENV === "production";
+export const REDIS_USER = process.env.REDIS_USER;
+export const REDIS_PASS = process.env.REDIS_PASS;
+export const PROD_HOST = process.env.PROD_HOST;
+export const PROD_PORT = process.env.PROD_PORT;
 
 // LeetCode
 export const CRED_URL = "https://leetcode.com";
