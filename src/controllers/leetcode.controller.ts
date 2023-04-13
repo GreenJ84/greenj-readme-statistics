c02a0ed74891dfe6cdd7386e8069f2e7829af759
Jesse Greenough <jesse.greenough84@gmail.com>
2023-04-13 00:30:29 -0700

    Implement leetcode user streak registration functionality

--- src/controllers/leetcode.controller.ts
+++ src/controllers/leetcode.controller.ts
@@ -10,7 +10,7 @@ import { THEMES } from '../utils/themes';
 import { preFlight, sleep } from "../utils/utils";
 import { parseDirect } from "../leetcode/apiParser";
 import {cardDirect, getGraph } from "../leetcode/leetcodeUtils";
-import { leetcodeGraphQL, preProbe, preQuery, updateUser } from '../leetcode/query';
+import { leetcodeGraphQL, preProbe, preQuery, streakQuery, updateStreak, updateUser } from '../leetcode/query';
 import { getCacheData, getCacheKey, setCacheData } from '../utils/cache';
 
 let sleepMod = -2;
@@ -86,7 +86,41 @@ export const leetcodeStats = async (req: Request, res: Response): Promise<void> 
 
 
 export const leetcodeStreakRegister = async (req: Request, res: Response) => {
-    
+    // PreFlight checks for user based routes
+    if (!preFlight(req, res)) {
+        return;
+    }
+    const cacheKey = getCacheKey(req);
+    // Try for cached data, Query API if not present
+    const [success, _] = await getCacheData(cacheKey);
+    if (success) {
+        res.status(208).json({
+            message: "User already registered",
+            code: "208"
+        });
+        return;
+    }
+
+    const queryResponse = await streakQuery(req)
+        .catch(err => {
+            throw err
+        });
+
+    const intervalId = setInterval(() => {
+        // console.log(intervalId);
+        updateStreak(cacheKey, intervalId, { ...req } as Request);
+    }, DATA_UDPDATE_INTERVAL);
+
+    await setCacheData(cacheKey, {
+        interval: intervalId,
+        data: queryResponse
+    });
+
+    res.status(201).json({
+        message: "User Registered",
+        code: "201"
+    });
+    return;
 }
 
 // User Streak specific controller
@@ -95,64 +129,28 @@ export const leetcodeStreak = async (req: Request, res: Response): Promise<void>
     if (!preFlight(req, res)) {
         return;
     }
-    const key = getCacheKey(req);
+    const cacheKey = getCacheKey(req);
     
-    let data: STREAKDATA;
-    const [success, cacheData] = await getCacheData(key);
+    const [success, cacheData] = await getCacheData(cacheKey);
     if (!success) {
-        const parseStreak = parseDirect(subRoute)
-        const path = getGraph(subRoute);
-        const preSet = await preProbe(req)
-        .catch(err => {
-            throw new ResponseError(
-                "Error build probe query for user membership length",
-                err, 502
-                )
-            });
-            const [membershipYears, csrf_credential] = preSet;
+        res.set('Content-Type', 'application/json');
+        res.status(401).json({
+            message: "User unauthorized. Registration required for API data.",
+            code: "401"
+        });
+        return;
+    }
+    const data = cacheData?.data as STREAKDATA;
             
-            const streakData: STREAKDATA = {
-                streak: [0, 0],
-                totalActive: 0,
-                mostActiveYear: 0,
-                completion: "0.00",
-                completionActuals: [0, 0],
-                theme: THEMES["black-ice"]!
-            }
-            const graphql = gql(
-                fs.readFileSync(path, 'utf8')
-                );
-                // Call the universal leetCode querier for each year
-                for (let year of membershipYears) {
-                    const data = await leetcodeGraphQL({
-                        query: graphql,
-                        variables: { username: req.params.username!, year: year }
-                    },
-                    GRAPHQL_URL,
-                    csrf_credential)
-                    .catch((err) => {
-                        throw new ResponseError("Error building LeetCode streak GraphQL query",
-                        err, 500,
-                        );
-                    });
-                    
-                    parseStreak(streakData, data, year);
-                }
-                setCacheData(key, streakData);
-                data = streakData;
-            } else {
-                data = cacheData as STREAKDATA;
-            }
-            
-            const streakCard = cardDirect(subRoute);
-            const card = streakCard(req, data);
+    const streakCard = cardDirect(req);
+    const card = streakCard(req, data);
 
-            res.status(200).send(card);
-            return;
+    res.status(200).send(card);
+    return;
 }
 
 export const leetcodeUnregister = async (req: Request, res: Response) => {
-
+    
 }
 
 export const leetcodeDaily = async (req: Request, res: Response): Promise<void> => {
