52eeb1b88aac68c1c96f780277087f0e138e3b5b
Jesse Greenough <jesse.greenough84@gmail.com>
2023-04-12 23:11:51 -0700

    Implement Registration functionality in routes

--- src/routes/github.routes.ts
+++ src/routes/github.routes.ts
@@ -1,8 +1,16 @@
 import express, {Request, Response, NextFunction} from "express";
 
-import {getCommitStreak, getProfileStats} from '../controllers/github.controller';
+import {getCommitStreak, getProfileStats, githubRegister, githubStreakRegister, githubUnregister} from '../controllers/github.controller';
 
 export const GithubRoutes = (app: express.Application) => {
+    // Register github user in server
+    app.get('/github/register/:username/profile',
+    (req: Request, res: Response, next: NextFunction) => {
+        githubRegister(req, res)
+            .catch(err => {
+                next(err)
+            })
+    });
     // Display github stats in trophies
     app.get('/github/trophies/:username',
         (req: Request, res: Response, next: NextFunction) => {
@@ -12,27 +20,46 @@ export const GithubRoutes = (app: express.Application) => {
         })
     });
     // Display github stats in Modal
-app.get('/github/stats/:username',
-    (req: Request, res: Response, next: NextFunction) => {
-        getProfileStats(req, res)
-        .catch(err => {
-            next(err)
-        })
-    });
+    app.get('/github/stats/:username',
+        (req: Request, res: Response, next: NextFunction) => {
+            getProfileStats(req, res)
+            .catch(err => {
+                next(err)
+            })
+        });
     // Display most used github languages
-app.get('/github/languages/:username',
+    app.get('/github/languages/:username',
+        (req: Request, res: Response, next: NextFunction) => {
+            getProfileStats(req, res)
+            .catch(err => {
+                next(err)
+            })
+        });
+
+    
+    // Register github user streak in server
+    app.get('/github/register/:username/streak',
     (req: Request, res: Response, next: NextFunction) => {
-        getProfileStats(req, res)
-        .catch(err => {
-            next(err)
-        })
+        githubStreakRegister(req, res)
+            .catch(err => {
+                next(err)
+            })
     });
     // Display github contributions streak
-app.get('/github/streak/:username',
+    app.get('/github/streak/:username',
+        (req: Request, res: Response, next: NextFunction) => {
+            getCommitStreak(req, res)
+            .catch(err => {
+                next(err)
+            })
+        });
+    
+    // Unegister github user in server
+    app.get('/github/unregister/:username',
     (req: Request, res: Response, next: NextFunction) => {
-        getCommitStreak(req, res)
-        .catch(err => {
-            next(err)
-        })
+        githubUnregister(req, res)
+            .catch(err => {
+                next(err)
+            })
     });
 }
