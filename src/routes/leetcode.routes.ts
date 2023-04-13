b5a5a2a8a3fa95960a5ef24d51697b8a59f0d8e1
Jesse Greenough <jesse.greenough84@gmail.com>
2023-04-12 23:46:54 -0700

    Implement routes for leetcode registration

--- src/routes/leetcode.routes.ts
+++ src/routes/leetcode.routes.ts
@@ -1,8 +1,16 @@
 import express, { Request, Response, NextFunction } from "express";
 
-import {leetcodeDaily, leetcodeStats} from "../controllers/leetcode.controller";
+import {leetcodeDaily, leetcodeRegister, leetcodeStats, leetcodeStreak, leetcodeStreakRegister, leetcodeUnregister} from "../controllers/leetcode.controller";
 
 export const LeetCodeRoutes = (app: express.Application) => {
+    // Register leetcode user in server
+    app.get('/leetcode/register/:username/profile',
+    (req: Request, res: Response, next: NextFunction) => {
+        leetcodeRegister(req, res)
+            .catch(err => {
+                next(err)
+            })
+    });
     // Stars, contribution points, reputaion, rating
     app.get('/leetcode/stats/:username',(req: Request, res: Response, next: NextFunction) => {
         leetcodeStats(req, res)
@@ -10,15 +18,6 @@ export const LeetCodeRoutes = (app: express.Application) => {
             next(err)
         })
     });
-
-    // Display LeetCode streak and activity
-    app.get('/leetcode/streak/:username',(req: Request, res: Response, next: NextFunction) => {
-        leetcodeStats(req, res)
-        .catch(err => {
-            next(err)
-        })
-    });
-
     // Display earned leetcode badges
     app.get('/leetcode/badges/:username',(req: Request, res: Response, next: NextFunction) => {
         leetcodeStats(req, res)
@@ -26,7 +25,6 @@ export const LeetCodeRoutes = (app: express.Application) => {
             next(err)
         })
     });
-
     // Get number of questions solved in total and by level
     app.get('/leetcode/questions_solved/:username',(req: Request, res: Response, next: NextFunction) => {
         leetcodeStats(req, res)
@@ -42,6 +40,32 @@ export const LeetCodeRoutes = (app: express.Application) => {
             next(err)
         })
     });
+
+
+    // Register for LeetCode streak
+    app.get('/leetcode/register/:username/streak',(req: Request, res: Response, next: NextFunction) => {
+        leetcodeStreakRegister(req, res)
+        .catch(err => {
+            next(err)
+        })
+    });
+    // Display LeetCode streak and activity
+    app.get('/leetcode/streak/:username',(req: Request, res: Response, next: NextFunction) => {
+        leetcodeStreak(req, res)
+        .catch(err => {
+            next(err)
+        })
+    });
+
+
+    // Unregister LeetCode user
+    app.get('/leetcode/unregister/:username',(req: Request, res: Response, next: NextFunction) => {
+        leetcodeUnregister(req, res)
+        .catch(err => {
+            next(err)
+        })
+    });
+
 
     // Get a random leetcode problem title
     app.get('/leetcode/daily-question', (req: Request, res: Response, next: NextFunction) => {
