0fa529be4b694fd41f906940f6e4759412a848e8
Jesse Greenough <jesse.greenough84@gmail.com>
2023-04-12 22:40:24 -0700

    Move Streak query functionality to query page

--- src/github/query.ts
+++ src/github/query.ts
@@ -6,8 +6,10 @@ import { gql } from "graphql-tag";
 import { match } from 'ts-pattern';
 
 import { GraphQLQuery, ResponseError, GIT_URL } from '../utils/constants';
-import { GraphQLResponse, StreakProbe } from './githubTypes';
+import { GraphQLResponse, STREAKTYPE, StreakProbe } from './githubTypes';
 import { setCacheData } from '../utils/cache';
+import { THEMES, THEMETYPE } from '../utils/themes';
+import { getResponseParse } from './apiParser';
 
 dotenv.config()
 
@@ -131,4 +133,58 @@ export const streakProbe = async (req: Request): Promise<[string , number[]]> =>
         (data as GraphQLResponse as StreakProbe).user.createdAt,
         [...(data as GraphQLResponse as StreakProbe).user.contributionsCollection.contributionYears].sort()
     ];
+}
+
+export const streakQuery = async (req: Request,created: string, years: number[]): Promise<STREAKTYPE> => {
+    // Start data with defaults sets
+    let streak: STREAKTYPE = {
+        total: 0,
+        totalText: "Total Contributions",
+        totalRange: [`${new Date(created as string).toISOString().slice(0, 10)}`, `${new Date().toISOString().slice(0, 10)}`],
+        curr: 0,
+        currText: "Current Streak",
+        currDate: ["", ""],
+        longest: 0,
+        longestText: "Longest Streak",
+        longestDate: ["", ""],
+        theme: req.query.theme !== undefined ?
+            THEMES[req.query.theme! as string] as THEMETYPE :
+            {
+                ...THEMES["black-ice"]!,
+                hideBorder: false,
+                borderRadius: 10,
+                locale: 'en'
+            } as THEMETYPE,
+    }
+    // starting template variables for query
+    const { username } = req.params;
+    let variables = { login: username, start: "", end: "" };
+
+    // Call data for each year
+    for (let year of years) {
+        // If before year is created year, set start to create data else year start
+        if (year == new Date(created as string).getFullYear()) {
+            variables.start = new Date(created as string).toISOString().slice(0, 19) + 'Z';
+        } else {
+            variables.start = `${year}-01-01T00:00:00Z`;
+        }
+        // If year is this year, set end to current date else end of year
+        if (year == new Date().getFullYear()) {
+            variables.end = new Date().toISOString().slice(0, 19) + 'Z'
+        } else {
+            variables.end = `${year}-12-31T00:00:00Z`;
+        }
+        // Query data for the specific yar
+        const data = await preQery(variables, "streak")
+            .then((data) => { return data })
+            .catch (err => {
+                throw err;
+            });
+        
+        // Get Function to parse data
+        const parse = getResponseParse(req);
+        // Parse that data with our current stats to update
+        parse(streak, data);
+    }
+    return streak;
 }
