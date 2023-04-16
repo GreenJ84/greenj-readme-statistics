21ad6e5693ce6b16bc35f30ad8b1cb3785f3c49e
Jesse Greenough <jesse.greenough84@gmail.com>
2023-04-16 13:48:07 -0700

    Update Github Typing convention

--- src/github/githubTypes.ts
+++ src/github/githubTypes.ts
@@ -1,65 +1,109 @@
-import { THEMETYPE } from "../utils/themes"
-
-export type GraphQLResponse =
-    StreakResponse |
-    StreakProbe |
-    StatsResponse
+import { ThemeType } from "../utils/themes"
 
 
-export type ReadMeData =
-    STREAKTYPE |
-    STATTYPE |
-    LANGTYPE
+export type GithRawGraphResponse =
+    GithRawUserProbe |
+    GithRawUserData
 
-// Streak Related Types
-export interface StreakProbe{
-    user: {
-        createdAt: string
-        contributionsCollection: {
-            contributionYears: number[]
+    export interface GithRawUserProbe{
+        user: {
+            createdAt: string
+            contributionsCollection: {
+                contributionYears: number[]
+            }
         }
-    }
-};
-export interface StreakResponse {
-    user: {
-        createdAt: string
-        contributionsCollection: {
-            contributionYears: number[]
-            contributionCalendar: {
-                weeks: {
-                    contributionDays: {
-                        contributionCount: number
-                        date: string
+    };
+
+export type GithRawUserData =
+    GithRawStreakData |
+    GithRawProfileData
+
+    export interface GithRawStreakData {
+        user: {
+            createdAt: string
+            contributionsCollection: {
+                contributionYears: number[]
+                contributionCalendar: {
+                    weeks: {
+                        contributionDays: {
+                            contributionCount: number
+                            date: string
+                        }[]
                     }[]
+                }
+            }
+        }
+    };
+    export interface GithRawProfileData{
+        user: {
+            name: string
+            contributionsCollection: {
+                totalCommitContributions: number
+                restrictedContributionsCount: number
+            }
+            repositoriesContributedTo: {
+                totalCount: number
+            }
+            pullRequests: {
+                totalCount: number
+            }
+            issues: {
+                totalCount: number
+            }
+            followers: {
+                totalCount: number
+            }
+            repositories: {
+                totalCount: number
+                nodes: {
+                    name: string
+                    stargazers: {
+                        totalCount: number
+                    }
+                    languages: {
+                        edges: {
+                            size: number
+                            node: {
+                                color: string
+                                name: string
+                            }
+                        }[]
+                    }
                 }[]
             }
         }
     }
-};
-    export interface contrWeek {
-        contributionDays: contrDay[]
-    }
-    export interface contrDay {
-        contributionCount: number
-        date: string
-    }
-export interface STREAKTYPE {
-    title?: string
-    total: number
-    totalText: string
-    totalRange: string[]
-    curr: number
-    currText: string
-    currDate: string[]
-    longest: number
-    longestText: string
-    longestDate: string[]
-    theme?: THEMETYPE
-}
 
 
-// Stats related types
-export interface StatsResponse{
+export type GithUserData =
+    GithStreak |
+    GithProfile
+
+    export interface GithStreak {
+        title?: string
+        total: number
+        totalText: string
+        totalRange: string[]
+        curr: number
+        currText: string
+        currDate: string[]
+        longest: number
+        longestText: string
+        longestDate: string[]
+        theme?: ThemeType
+    }
+
+export type GithRawProfile = 
+    GithRawUserStats |
+    GithRawUserLanguages |
+    GithRawUserBadges
+
+export type GithProfile =
+    GithUserStats |
+    GithUserLanguages | 
+    GithUserBadges
+
+export interface GithRawUserStats{
     user: {
         name: string
         contributionsCollection: {
@@ -89,7 +133,7 @@ export interface StatsResponse{
         }
     }
 }
-export interface STATTYPE{
+export interface GithUserStats{
     title?: string
     grade?: [string, number]
     stars: number
@@ -99,12 +143,12 @@ export interface STATTYPE{
     issues: number
     contributedTo: number,
     repos: number
-    theme?: THEMETYPE
+    theme?: ThemeType
 }
 
 
 // Language related types
-export interface LangsResponse{
+export interface GithRawUserLanguages{
     user: {
         repositories: {
             nodes: {
@@ -122,16 +166,23 @@ export interface LangsResponse{
         }
     }
 }
-export interface Language {
-    name: string
-    usage: number | string
-    position: number
-    width: number
-    color: string
-}
-export interface LANGTYPE {
+export interface GithUserLanguages {
     title?: string
     totalSize: number
-    languages: Language[]
-    theme?: THEMETYPE
+    languages: {
+        name: string
+        usage: number | string
+        position: number
+        width: number
+        color: string
+    }[]
+    theme?: ThemeType
+}
+
+export interface GithRawUserBadges {
+
+}
+
+export interface GithUserBadges {
+
 }
