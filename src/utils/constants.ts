066d246b0e872b4818f676d9c2962f21ef5c5213
Jesse Greenough <jesse.greenough84@gmail.com>
2023-04-16 14:25:51 -0700

    Move github token to global constants file

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -11,6 +11,7 @@ export const REDIS_USER = process.env.REDIS_USER;
 export const REDIS_PASS = process.env.REDIS_PASS;
 export const PROD_HOST = process.env.PROD_HOST;
 export const PROD_PORT = process.env.PROD_PORT;
+export const GITHUB_TOKEN = process.env.GITHUB_TOKEN;
 
 // LeetCode
 export const LEET_CRED_URL = "https://leetcode.com";
