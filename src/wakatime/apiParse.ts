c6d3c2ca7f1f91855431442efad76c2b0e1d9e6f
Jesse Greenough <jesse.greenough84@gmail.com>
2023-04-17 10:52:12 -0700

    Update wakatime data parsing

--- src/wakatime/apiParse.ts
+++ src/wakatime/apiParse.ts
@@ -1,70 +1,22 @@
 /** @format */
 
-import { match } from "ts-pattern";
-
 import {
   WakaInsight,
   WakaLang,
   WakaStat,
   WakaRawData,
-  WakaRawSection,
-  WakaProfileData,
-  WakaTopic,
 } from "./wakatimeTypes";
 import { getLangColor } from "../utils/colors";
 
-export const wakaParseDirect = (type: string): Function => {
-  const parseFunc: Function = match(type)
-    .with("insights", () => {
-      return wakaInsightParse;
-    })
-    .with("languages", () => {
-      return wakaLanguagesParse;
-    })
-    .with("stats", () => {
-      return wakaStatParse;
-    })
-    .run();
+export const wakaRawParse = (data: WakaRawData): [WakaInsight, WakaLang, WakaStat] => {
+  let insights = wakaInsightParse(data);
+  let languages = wakaLanguagesParse(data);
+  let stats = wakaStatParse(data);
 
-  return parseFunc;
+  return [insights, languages, stats];
 };
 
-const wakaTopicShave = (section: WakaRawSection[]): WakaTopic[] => {
-  let shavedSection: WakaTopic[] = [];
-  for (let top of section) {
-    const { name, percent, total_seconds } = top;
-    shavedSection.push({ name, percent, total_seconds });
-  }
-  return shavedSection;
-};
-
-export const wakaRawShave = (data: WakaRawData): WakaProfileData => {
-  let shavedData = {
-    start: data.start,
-    daily_average: data.daily_average,
-    daily_average_including_other_language:
-      data.daily_average_including_other_language,
-    days_minus_holidays: data.days_minus_holidays,
-    total_seconds: data.total_seconds,
-    total_seconds_including_other_language:
-      data.total_seconds_including_other_language,
-    categories: wakaTopicShave(data.categories.slice(0, 3)),
-    projects: wakaTopicShave(data.projects.slice(0, 3)),
-    languages: wakaTopicShave(data.languages.slice(0, 3)),
-    editors: wakaTopicShave(data.editors.slice(0, 3)),
-    operating_systems: wakaTopicShave(data.operating_systems.slice(0, 3)),
-    dependencies: wakaTopicShave(data.dependencies.slice(0, 3)),
-    machines: wakaTopicShave(data.machines.slice(0, 3)),
-    best_day: {
-      date: data.best_day.date,
-      total_seconds: data.best_day.total_seconds,
-    },
-  };
-
-  return shavedData;
-};
-
-const wakaInsightParse = (data: WakaProfileData): WakaInsight => {
+const wakaInsightParse = (data: WakaRawData): WakaInsight => {
   const topLanguage = data.languages[0];
   const topProject = data.projects[0];
   const topCategory = data.categories[0];
@@ -86,7 +38,7 @@ const wakaInsightParse = (data: WakaProfileData): WakaInsight => {
   } as WakaInsight;
 };
 
-const wakaLanguagesParse = (data: WakaProfileData): WakaLang => {
+const wakaLanguagesParse = (data: WakaRawData): WakaLang => {
   let topTotal = 0;
   data.languages.slice(0, 6).map((lang) => {
     topTotal += lang.total_seconds;
@@ -106,7 +58,7 @@ const wakaLanguagesParse = (data: WakaProfileData): WakaLang => {
   } as WakaLang;
 };
 
-const wakaStatParse = (data: WakaProfileData): WakaStat => {
+const wakaStatParse = (data: WakaRawData): WakaStat => {
   const totalBest = (data.best_day.total_seconds / 60 / 60).toFixed(1);
   const bestDate = data.best_day.date;
   const totalDevSec = (
