9817d0f52c6924c391a2bc024a08bc753884b2e3
Jesse Greenough <jesse.greenough84@gmail.com>
2023-06-27 01:52:36 -0700

    Implement request query handling to remove cache limiting on routes called from my index HTML server page

--- src/routes/github.routes.ts
+++ src/routes/github.routes.ts
@@ -1,65 +1,95 @@
-import express, {Request, Response, NextFunction} from "express";
+/** @format */
 
-import {getCommitStreak, getProfileStats, githubRegister, githubStreakRegister, githubUnregister} from '../controllers/github.controller';
+import express, { Request, Response, NextFunction } from "express";
+
+import {
+  getCommitStreak,
+  getProfileStats,
+  githubRegister,
+  githubStreakRegister,
+  githubUnregister,
+} from "../controllers/github.controller";
 
 export const GithubRoutes = (app: express.Application) => {
-    // Register github user in server
-    app.get('/github/register/:username',
+  // Register github user in server
+  app.get(
+    "/github/register/:username",
     (req: Request, res: Response, next: NextFunction) => {
-        githubRegister(req, res)
-            .catch(err => {
-                next(err)
-            })
-    });
-    // Display github stats in trophies
-    app.get('/github/trophies/:username',
-        (req: Request, res: Response, next: NextFunction) => {
-        getProfileStats(req, res)
-        .catch(err => {
-            next(err)
-        })
-    });
-    // Display github stats in Modal
-    app.get('/github/stats/:username',
-        (req: Request, res: Response, next: NextFunction) => {
-            getProfileStats(req, res)
-            .catch(err => {
-                next(err)
-            })
-        });
-    // Display most used github languages
-    app.get('/github/languages/:username',
-        (req: Request, res: Response, next: NextFunction) => {
-            getProfileStats(req, res)
-            .catch(err => {
-                next(err)
-            })
-        });
+      githubRegister(req, res).catch((err) => {
+        next(err);
+      });
+    }
+  );
+  // Display github stats in trophies
+  app.get(
+    "/github/trophies/:username",
+    (req: Request, res: Response, next: NextFunction) => {
+      // Allow Theme updates on Server Landing page
+      if (req.query.docsDisplay == "true") {
+        res.cacheControl = { noCache: true };
+      }
+      getProfileStats(req, res).catch((err) => {
+        next(err);
+      });
+    }
+  );
+  // Display github stats in Modal
+  app.get(
+    "/github/stats/:username",
+    (req: Request, res: Response, next: NextFunction) => {
+      // Allow Theme updates on Server Landing page
+      if (req.query.docsDisplay == "true") {
+        res.cacheControl = { noCache: true };
+      }
+      getProfileStats(req, res).catch((err) => {
+        next(err);
+      });
+    }
+  );
+  // Display most used github languages
+  app.get(
+    "/github/languages/:username",
+    (req: Request, res: Response, next: NextFunction) => {
+      // Allow Theme updates on Server Landing page
+      if (req.query.docsDisplay == "true") {
+        res.cacheControl = { noCache: true };
+      }
+      getProfileStats(req, res).catch((err) => {
+        next(err);
+      });
+    }
+  );
 
+  // Register github user streak in server
+  app.get(
+    "/github/streak/register/:username",
+    (req: Request, res: Response, next: NextFunction) => {
+      githubStreakRegister(req, res).catch((err) => {
+        next(err);
+      });
+    }
+  );
+  // Display github contributions streak
+  app.get(
+    "/github/streak/:username",
+    (req: Request, res: Response, next: NextFunction) => {
+      // Allow Theme updates on Server Landing page
+      if (req.query.docsDisplay == "true") {
+        res.cacheControl = { noCache: true };
+      }
+      getCommitStreak(req, res).catch((err) => {
+        next(err);
+      });
+    }
+  );
 
-    // Register github user streak in server
-    app.get('/github/streak/register/:username',
+  // Unegister github user in server
+  app.get(
+    "/github/unregister/:username",
     (req: Request, res: Response, next: NextFunction) => {
-        githubStreakRegister(req, res)
-            .catch(err => {
-                next(err)
-            })
-    });
-    // Display github contributions streak
-    app.get('/github/streak/:username',
-        (req: Request, res: Response, next: NextFunction) => {
-            getCommitStreak(req, res)
-            .catch(err => {
-                next(err)
-            })
-        });
-    
-    // Unegister github user in server
-    app.get('/github/unregister/:username',
-    (req: Request, res: Response, next: NextFunction) => {
-        githubUnregister(req, res)
-            .catch(err => {
-                next(err)
-            })
-    });
-}
+      githubUnregister(req, res).catch((err) => {
+        next(err);
+      });
+    }
+  );
+};
--- src/routes/leetcode.routes.ts
+++ src/routes/leetcode.routes.ts
@@ -1,77 +1,124 @@
+/** @format */
+
 import express, { Request, Response, NextFunction } from "express";
 
-import {leetcodeDaily, leetcodeRegister, leetcodeStats, leetcodeStreak, leetcodeStreakRegister, leetcodeUnregister} from "../controllers/leetcode.controller";
+import {
+  leetcodeDaily,
+  leetcodeRegister,
+  leetcodeStats,
+  leetcodeStreak,
+  leetcodeStreakRegister,
+  leetcodeUnregister,
+} from "../controllers/leetcode.controller";
 
 export const LeetCodeRoutes = (app: express.Application) => {
-    // Register leetcode user in server
-    app.get('/leetcode/register/:username',
+  // Register leetcode user in server
+  app.get(
+    "/leetcode/register/:username",
     (req: Request, res: Response, next: NextFunction) => {
-        leetcodeRegister(req, res)
-            .catch(err => {
-                next(err)
-            })
-    });
-    // Stars, contribution points, reputaion, rating
-    app.get('/leetcode/stats/:username',(req: Request, res: Response, next: NextFunction) => {
-        leetcodeStats(req, res)
-        .catch(err => {
-            next(err)
-        })
-    });
-    // Display earned leetcode badges
-    app.get('/leetcode/badges/:username',(req: Request, res: Response, next: NextFunction) => {
-        leetcodeStats(req, res)
-        .catch(err => {
-            next(err)
-        })
-    });
-    // Get number of questions solved in total and by level
-    app.get('/leetcode/completion/:username',(req: Request, res: Response, next: NextFunction) => {
-        leetcodeStats(req, res)
-        .catch(err => {
-            next(err)
-        })
-    });
+      leetcodeRegister(req, res).catch((err) => {
+        next(err);
+      });
+    }
+  );
+  // Stars, contribution points, reputaion, rating
+  app.get(
+    "/leetcode/stats/:username",
+    (req: Request, res: Response, next: NextFunction) => {
+      // Allow Theme updates on Server Landing page
+      if (req.query.docsDisplay == "true") {
+        res.cacheControl = { noCache: true };
+      }
+      leetcodeStats(req, res).catch((err) => {
+        next(err);
+      });
+    }
+  );
+  // Display earned leetcode badges
+  app.get(
+    "/leetcode/badges/:username",
+    (req: Request, res: Response, next: NextFunction) => {
+      // Allow Theme updates on Server Landing page
+      if (req.query.docsDisplay == "true") {
+        res.cacheControl = { noCache: true };
+      }
+      leetcodeStats(req, res).catch((err) => {
+        next(err);
+      });
+    }
+  );
+  // Get number of questions solved in total and by level
+  app.get(
+    "/leetcode/completion/:username",
+    (req: Request, res: Response, next: NextFunction) => {
+      // Allow Theme updates on Server Landing page
+      if (req.query.docsDisplay == "true") {
+        res.cacheControl = { noCache: true };
+      }
+      leetcodeStats(req, res).catch((err) => {
+        next(err);
+      });
+    }
+  );
 
-    // Display top 5 most recent questions solved and their languages
-    app.get('/leetcode/submission/:username',(req: Request, res: Response, next: NextFunction) => {
-        leetcodeStats(req, res)
-        .catch(err => {
-            next(err)
-        })
-    });
+  // Display top 5 most recent questions solved and their languages
+  app.get(
+    "/leetcode/submission/:username",
+    (req: Request, res: Response, next: NextFunction) => {
+      // Allow Theme updates on Server Landing page
+      if (req.query.docsDisplay == "true") {
+        res.cacheControl = { noCache: true };
+      }
+      leetcodeStats(req, res).catch((err) => {
+        next(err);
+      });
+    }
+  );
 
+  // Register for LeetCode streak
+  app.get(
+    "/leetcode/streak/register/:username",
+    (req: Request, res: Response, next: NextFunction) => {
+      leetcodeStreakRegister(req, res).catch((err) => {
+        next(err);
+      });
+    }
+  );
+  // Display LeetCode streak and activity
+  app.get(
+    "/leetcode/streak/:username",
+    (req: Request, res: Response, next: NextFunction) => {
+      // Allow Theme updates on Server Landing page
+      if (req.query.docsDisplay == "true") {
+        res.cacheControl = { noCache: true };
+      }
+      leetcodeStreak(req, res).catch((err) => {
+        next(err);
+      });
+    }
+  );
 
-    // Register for LeetCode streak
-    app.get('/leetcode/streak/register/:username',(req: Request, res: Response, next: NextFunction) => {
-        leetcodeStreakRegister(req, res)
-        .catch(err => {
-            next(err)
-        })
-    });
-    // Display LeetCode streak and activity
-    app.get('/leetcode/streak/:username',(req: Request, res: Response, next: NextFunction) => {
-        leetcodeStreak(req, res)
-        .catch(err => {
-            next(err)
-        })
-    });
+  // Unregister LeetCode user
+  app.get(
+    "/leetcode/unregister/:username",
+    (req: Request, res: Response, next: NextFunction) => {
+      leetcodeUnregister(req, res).catch((err) => {
+        next(err);
+      });
+    }
+  );
 
-
-    // Unregister LeetCode user
-    app.get('/leetcode/unregister/:username',(req: Request, res: Response, next: NextFunction) => {
-        leetcodeUnregister(req, res)
-        .catch(err => {
-            next(err)
-        })
-    });
-
-
-    // Get a random leetcode problem title
-    app.get('/leetcode/daily', (req: Request, res: Response, next: NextFunction) => {
-        leetcodeDaily(req, res)
-        .catch(err => {
-            next(err)
-        })
-    });
-}
+  // Get a random leetcode problem title
+  app.get(
+    "/leetcode/daily",
+    (req: Request, res: Response, next: NextFunction) => {
+      // Allow Theme updates on Server Landing page
+      if (req.query.docsDisplay == "true") {
+        res.cacheControl = { noCache: true };
+      }
+      leetcodeDaily(req, res).catch((err) => {
+        next(err);
+      });
+    }
+  );
+};
