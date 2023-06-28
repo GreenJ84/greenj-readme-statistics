2b8185f3c42f3444562608996915a665231f41ed
Jesse Greenough <jesse.greenough84@gmail.com>
2023-06-28 01:25:09 -0700

    Implement new form input into customization logic

--- public/index.js
+++ public/index.js
@@ -4,6 +4,11 @@ const usernameInput = document.getElementById("username");
 const themeSelect = document.getElementById("themeSelect");
 const borderRadiusInput = document.getElementById("borderRadius");
 const hideBorderInput = document.getElementById("hideBorder");
+
+const backgroundColorInput = document.getElementById("backgroundColor");
+const borderColorInput = document.getElementById("borderColor");
+const strokeColorInput = document.getElementById("strokeColor");
+
 const update = document.getElementById("update");
 
 const Themes = await fetch("/themes")
@@ -24,21 +29,37 @@ for (let theme of Themes.themes) {
 update.addEventListener("click", (e) => {
   e.preventDefault();
   let modals = document.getElementsByClassName("svgModal");
+  console.log(location.hostname);
+  console.log(location.href);
+  console.log(location.origin);
 
   for (let modal of modals) {
+    // Remove old query string and Username
     let old = modal.src.split("/");
     old.pop();
     old = old.join("/");
 
-    const username = usernameInput.value ?? "GreenJ84";
+    // Get static customizations
+    const username = usernameInput.value ? usernameInput.value : "GreenJ84";
     const theme = themeSelect.value;
     const hideBorder =
-      hideBorderInput.value == "true" ? "&hideBorder=true" : "";
+      hideBorderInput.value == "true" ? "&hideBorder=true" : "&hideBorder=false";
     const borderRadius = borderRadiusInput.value;
 
-    console.log(
-      `${old}/${username}?docsDisplay=true&theme=${theme}${hideBorder}&borderRadius=${borderRadius}`
-    );
-    modal.src = `${old}/${username}?docsDisplay=true&theme=${theme}${hideBorder}&borderRadius=${borderRadius}`;
+    // Get extra customizations
+    const backgroundColor = backgroundColorInput.value
+      ? `&background=${backgroundColorInput.value}`
+      : "";
+    const borderColor = borderColorInput.value
+      ? `&border=${borderColorInput.value}`
+      : "";
+    const strokeColor = strokeColorInput.value
+      ? `&stroke=${strokeColorInput.value}`
+      : "";
+
+    const imgString = `${old}/${username}?docsDisplay=true&theme=${theme}&borderRadius=${borderRadius}${hideBorder}${backgroundColor}${borderColor}${strokeColor}`;
+
+    console.log(imgString);
+    modal.src = imgString;
   }
 });
