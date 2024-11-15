85aaf85246d99a63a94b631539591ea82318ada9
Jesse Greenough <jesse.greenough84@gmail.com>
2024-11-14 22:42:07 -0800

    Remove old files for migrated functionality

--- old-src/utils/constants.ts
+++ old-src/utils/constants.ts
@@ -1,16 +1,9 @@
 /** @format */
-
-import { DocumentNode } from "graphql";
 import dotenv from "dotenv";
 
 dotenv.config();
 
 // Environment Variables
-export const PRODUCTION = process.env.NODE_ENV === "production";
-export const REDIS_USER = process.env.REDIS_USER;
-export const REDIS_PASS = process.env.REDIS_PASS;
-export const PROD_HOST = process.env.PROD_HOST;
-export const PROD_PORT = process.env.PROD_PORT;
 export const GITHUB_TOKEN = process.env.GITHUB_TOKEN;
 
 // LeetCode
@@ -25,26 +18,3 @@ export const GIT_URL = "https://api.github.com/graphql";
 //WakaTime
 export const WAKA_TIME_URL = "https://wakatime.com/api/v1";
 export const WAKA_TIME_AUTH_URL = "https://wakatime.com/oauth/token";
-
-
-// Production update is 8hrs
-// Development update is 2 min
-export const DATA_UDPDATE_INTERVAL = PRODUCTION
-  ? 1000 * 60 * 60 * 8
-  : 1000 * 60 * 2;
-
-export class ResponseError extends Error {
-  error: any;
-  error_code: number;
-
-  constructor(message: string, error: any, error_code: number) {
-    super(message);
-    this.error = error;
-    this.error_code = error_code;
-  }
-}
-
-export interface GraphQuery {
-  variables?: { [key: string]: unknown };
-  query: DocumentNode;
-}
