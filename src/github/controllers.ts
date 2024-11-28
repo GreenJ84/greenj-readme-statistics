6a30808004f3d82ed36d74db8f9a82780a28e354
Jesse Greenough <jesse.greenough84@gmail.com>
2024-11-27 22:01:11 -0800

    Create a controller function to retrieve user's data from redis cache, or poll from Github if not found

---
+++ src/github/controllers.ts
@@ -0,0 +1,52 @@
+/** @format */
+
+import { Request, Response } from "express";
+
+import { cache } from "../server";
+import { UserData } from "./types";
+import { getGithubParserFunction } from "./utils";
+import { GithubQuerier } from "./platformQuerier";
+
+const PLATFORM = "github";
+const querier = new GithubQuerier();
+
+export const getProfileData = async (
+  req: Request,
+  res: Response,
+  subRoute: string
+) => {
+  const username = req.params.username!;
+  const cacheKey = `${PLATFORM}:${username}:${subRoute}`;
+
+  let cacheData = await cache.getItem(cacheKey);
+  if (cacheData === null) {
+    cacheData = await querier.getUserData( username, subRoute)
+      .catch((err) => {
+        throw err;
+      });
+
+    (async () => {
+      await cache.setItem(cacheKey, cacheData)
+        .catch((err) => {
+          console.error("Error setting cache:", err);
+        });
+    })();
+  }
+
+  const data = cacheData as UserData;
+
+  const createCard: Function = getGithubParserFunction(subRoute);
+  const card: string = createCard(req, data);
+
+  res.setHeader("Content-Type", "image/svg+xml");
+  res.status(200).send(card);
+};
+
+export const register = async (req: Request, res: Response) => {
+
+};
+
+
+export const unregister = async (req: Request, res: Response) => {
+
+};
