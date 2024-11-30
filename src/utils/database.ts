ae75df3486b528b304d173daf5b8e4a7190818e7
Jesse Greenough <jesse.greenough84@gmail.com>
2024-11-30 12:43:27 -0800

    Implement functionality for registering and un-registering the user on the local sqlite db

--- src/utils/database.ts
+++ src/utils/database.ts
@@ -1,5 +1,6 @@
 import path from "path";
 import Database, { Database as TDatabase } from "better-sqlite3";
+import { ResponseError } from "./utils";
 
 export class PlatformDb {
   private _db: TDatabase;
@@ -23,6 +24,27 @@ export class PlatformDb {
     `);
   }
 
+  registerUser(username: string): boolean {
+    const user = this._db.prepare(`SELECT id FROM users WHERE username = ?`).get(username);
+    if (user === undefined){
+      try{
+        this._db.prepare(`INSERT INTO users (username) VALUES (?)`).run(username);
+        return true;
+      } catch (err) {
+        throw new ResponseError(`Error registering user ${username}`, err, 500);
+      }
+    }
+    return false;
+  };
+
+  unregisterUser(username: string): void{
+    try{
+      this._db.prepare(`DELETE FROM users WHERE username = ?`).run(username);
+    } catch (err) {
+      throw new ResponseError(`Error unregistering user ${username})`, err, 500);
+    }
+  }
+
   updateRegisteredUsers(
     dataUpdate: (user: string) => void
   ){
@@ -34,7 +56,7 @@ export class PlatformDb {
     });
   }
 
-  refreshUpdateTime(id: number): boolean{
+  private refreshUpdateTime(id: number): boolean{
     try {
       this._db.prepare(`
         UPDATE users
