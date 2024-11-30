2a512e65b615adf3bbca8626c1dacb675f639015
Jesse Greenough <jesse.greenough84@gmail.com>
2024-11-30 12:38:21 -0800

    Implement full profile querying method on Github Querier - Separate functionality of post-probe streak querying to utilize in profile or alone streak requests.

--- src/github/platformQuerier.ts
+++ src/github/platformQuerier.ts
@@ -3,7 +3,7 @@ import { match } from "ts-pattern";
 import { ApolloClient, gql, InMemoryCache } from "@apollo/client";
 
 import { GraphQuery, ResponseError } from "../utils/utils";
-import { RawGraphResponse, RawStreakData, RawUserData, RawUserLanguages, RawUserProbe, RawUserStats, UserData, UserLanguages, UserProfile, UserStats, UserStreak } from "./types";
+import { RawGraphResponse, RawProfileData, RawStreakData, RawUserData, RawUserLanguages, RawUserProbe, RawUserStats, UserData, UserLanguages, UserProfile, UserStats, UserStreak } from "./types";
 
 import { GITHUB_TOKEN } from "./github_environment";
 import { getGraphQuery } from "./utils";
@@ -65,8 +65,31 @@ export class GithubQuerier {
 
   private profileQueryInProgress: Record<string, Boolean> = {};
   private async getUserProfile(username: string): Promise<UserProfile>{
-    let profile = {} as UserProfile;
-    return profile;
+    if (this.profileQueryInProgress[username]) {
+      throw new ResponseError("This call occurred while query resources were already being used. Try again after a moment.", "Resource Conflicts", 409);
+    }
+    this.profileQueryInProgress[username] = true;
+    const now = new Date().toISOString();
+    const variables = {
+      login: username,
+      start: `${now.slice(0, 4)}-01-01T00:00:00Z`,
+      end: now.slice(0, 19),
+    };
+    const response = await this.querySetup(variables, "all")
+      .then((data: RawUserData) => {
+        return data as RawProfileData;
+      })
+      .catch((err) => {
+        this.statsQueryInProgress[username] = false;
+        throw err;
+      });
+
+    this.profileQueryInProgress[username] = false;
+    return {
+      streak: await this.captureStreakFromProbeData(response as RawUserProbe, username),
+      stats: statsParse(response as RawUserStats),
+      languages: langsParse(response as RawUserLanguages),
+    };
   }
 
   private statsQueryInProgress: Record<string, Boolean> = {};
@@ -86,7 +109,8 @@ export class GithubQuerier {
         throw err;
       });
 
-    return statsParse(queryResponse);
+      this.statsQueryInProgress[username] = false;
+      return statsParse(queryResponse);
   }
 
   private langsQueryInProgress: Record<string, Boolean> = {};
@@ -106,12 +130,13 @@ export class GithubQuerier {
         throw err;
       });
 
-    return langsParse(queryResponse);
+      this.statsQueryInProgress[username] = false;
+      return langsParse(queryResponse);
   }
 
   async streakProbe(
     username: string
-  ): Promise<[string, number[]]> {
+  ): Promise<RawUserProbe> {
     const now = new Date().toISOString();
     const today = now.slice(0, 19);
     const year = now.slice(0, 4);
@@ -136,10 +161,7 @@ export class GithubQuerier {
         );
       });
   
-    return [
-      data.user.createdAt,
-      [...data.user.contributionsCollection.contributionYears].sort(),
-    ];
+    return data;
   };
 
   private streakQueryInProgress: Record<string, Boolean> = {};
@@ -149,12 +171,28 @@ export class GithubQuerier {
     }
     this.streakQueryInProgress[username] = true;
 
-    const [created, years] = await this.streakProbe(username).catch(
+    const probeData = await this.streakProbe(username).catch(
       (err) => {
         this.streakQueryInProgress[username] = false;
         throw err;
       }
     );
+    try {
+      const streakPromise = this.captureStreakFromProbeData(probeData, username);
+      this.streakQueryInProgress[username] = false;
+      return streakPromise;
+    } catch (err) {
+      console.error(`Error capturing streak for ${username}:`, err);
+      this.streakQueryInProgress[username] = false;
+      throw err;
+    }
+  }
+
+  private async captureStreakFromProbeData(data: RawUserProbe, username: string): Promise<UserStreak>{
+    const [created, years] = [
+      data.user.createdAt,
+      [...data.user.contributionsCollection.contributionYears].sort()
+    ];
 
     let streak: UserStreak = {
       total: 0,
@@ -173,28 +211,24 @@ export class GithubQuerier {
 
     let variables = { login: username, start: "", end: "" };
     for (let year of years) {
-      // If before year is created year, set start to create data else year start
+      // If before year is created, set start to create date else Jan 1st midnight
       if (year == new Date(created as string).getFullYear()) {
         variables.start =
           new Date(created as string).toISOString().slice(0, 19) + "Z";
       } else {
         variables.start = `${year}-01-01T00:00:00Z`;
       }
-      // If year is this year, set end to current date else end of year
+      // If year is this year, set end to current date else Dec 31st 11:59
       if (year == new Date().getFullYear()) {
         variables.end = new Date().toISOString().slice(0, 19) + "Z";
       } else {
-        variables.end = `${year}-12-31T00:00:00Z`;
+        variables.end = `${year}-12-31T11:59:59Z`;
       }
       // Query data for the specific yar
       const queryResponse = await this.querySetup(variables, "streak")
-      .then((data: RawUserData) => {
-        return data as RawStreakData;
-      })
-      .catch((err) => {
-        this.streakQueryInProgress[username] = false;
-        throw err;
-      });
+        .then((data: RawUserData) => {
+          return data as RawStreakData;
+        })
 
       // Parse that data with our current stats to update
       streak = streakParse(queryResponse, streak);
@@ -206,7 +240,7 @@ export class GithubQuerier {
   getUserData(route: string): (username: string)  => Promise<UserData>
   {
     return match(route)
-      .with("all", () =>
+      .with("profile", () =>
         this.getUserProfile.bind(this)
       )
       .with("stats", () =>
